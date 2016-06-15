2a3e7cec74b0ff1bba312389fb86d339b54502d6
Hiroyuki Adachi <adac@mtj.biglobe.ne.jp>
2016-06-15 07:55:55 -0700

    lands list

--- app/models/home.rb
+++ app/models/home.rb
@@ -27,7 +27,8 @@ class Home < ActiveRecord::Base
   REG_PHONE = /\A\d{2,4}-\d{2,4}-\d{4}\z/
 
   has_many :workers,  ->{order(:display_order)}
-  has_many :lands,    ->{order(:place)}
+  has_many :owned_lands,    ->{order(:place)}, {class_name: :Land, foreign_key: :owner_id}
+  has_many :managed_lands,  ->{order(:place)}, {class_name: :Land, foreign_key: :manager_id}
   
   belongs_to :holder, -> {with_deleted}, {class_name: :Worker, foreign_key: :worker_id}
   belongs_to :section
