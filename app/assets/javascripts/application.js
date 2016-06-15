8df74d32ebf6a26c482cfe2df24d41278d667b5c
Hiroyuki-Adachi <hiroyuki.adachi55@gmail.com>
2016-06-15 10:52:54 +0900

    jQuery UIのタッチパネル対応

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require jquery-ui/autocomplete
 //= require jquery-ui/sortable
+//= require jquery.ui.touch-punch
 //= require i18n
 //= require i18n/translations
 //= require dateformat
