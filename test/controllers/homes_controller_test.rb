f96d00f7dcc7b9b0bbd76301659d22ad73bc8af5
Hiroyuki-Adachi <hiroyuki.adachi55@gmail.com>
2016-06-16 13:00:07 +0900

    世帯／作業者のテスト

---
+++ test/controllers/homes_controller_test.rb
@@ -0,0 +1,43 @@
+require 'test_helper'
+
+class HomesControllerTest < ActionController::TestCase
+  setup do
+    @home = Home.first
+    @update = { name: "試験", phonetic: "しけん", section_id: 1, member_flag: true, display_order: 99 }
+  end
+
+  test "世帯マスタ一覧" do
+    get :index
+    assert_response :success
+  end
+
+  test "世帯マスタ新規作成(表示)" do
+    get :new
+    assert_response :success
+  end
+
+  test "世帯マスタ新規作成(実行)" do
+    assert_difference('Home.count') do
+      post :create, home: @update
+    end
+
+    assert_redirected_to homes_path
+  end
+
+  test "世帯マスタ変更(表示)" do
+    get :edit, id: @home
+    assert_response :success
+  end
+
+  test "世帯マスタ変更(実行)" do
+    patch :update, id: @home, home: @update
+    assert_redirected_to homes_path
+  end
+
+  test "世帯マスタ削除" do
+    assert_difference('Home.count', -1) do
+      delete :destroy, id: @home
+    end
+    assert_redirected_to homes_path
+  end
+end
---
+++ test/controllers/workers_controller_test.rb
@@ -0,0 +1,46 @@
+require 'test_helper'
+
+class WorkersControllerTest < ActionController::TestCase
+  setup do
+    @worker = Worker.first
+    @update = { 
+        family_name: "試験", first_name: "太郎", family_phonetic: "しけん", first_phonetic: "たろう",
+        home_id: Home.first, display_order: 99 
+    }
+  end
+
+  test "作業者マスタ一覧" do
+    get :index
+    assert_response :success
+  end
+
+  test "作業者マスタ新規作成(表示)" do
+    get :new
+    assert_response :success
+  end
+
+  test "作業者マスタ新規作成(実行)" do
+    assert_difference('Worker.count') do
+      post :create, worker: @update
+    end
+
+    assert_redirected_to workers_path
+  end
+
+  test "作業者マスタ変更(表示)" do
+    get :edit, id: @worker
+    assert_response :success
+  end
+
+  test "作業者マスタ変更(実行)" do
+    patch :update, id: @worker, worker: @update
+    assert_redirected_to workers_path
+  end
+
+  test "作業者マスタ削除" do
+    assert_difference('Worker.count', -1) do
+      delete :destroy, id: @worker
+    end
+    assert_redirected_to workers_path
+  end
+end
