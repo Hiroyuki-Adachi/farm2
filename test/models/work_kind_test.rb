78297b11fada466fcdc753b511fa01ad1144261f
Hiroyuki Adachi <adac@mtj.biglobe.ne.jp>
2016-06-18 05:48:33 -0700

    作業種別単価テスト

---
+++ test/models/work_kind_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class WorkKindTest < ActiveSupport::TestCase
+  test "各年の単価設定" do
+    work_kind = WorkKind.find(work_kinds(:work_kind_every_term).id) 
+    assert_equal 2010, work_kind.term_price(2010)
+    assert_equal 2015, work_kind.term_price(2015)
+  end
+
+  test "初年度のみ単価設定" do
+    work_kind = WorkKind.find(work_kinds(:work_kind_first_term).id) 
+    assert_equal work_kind_prices(:work_kind_prices_first_0).price, work_kind.term_price(2010)
+    assert_equal work_kind_prices(:work_kind_prices_first_0).price, work_kind.term_price(2015)
+  end
+end
