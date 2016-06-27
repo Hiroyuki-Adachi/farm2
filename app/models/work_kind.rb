e9ccb6e3ecc78e9a485c5bf49597262851a89354
Hiroyuki-Adachi <hiroyuki.adachi55@gmail.com>
2016-06-27 12:24:49 +0900

    作業料金キャッシュ対応

--- app/models/work_kind.rb
+++ app/models/work_kind.rb
@@ -36,31 +36,42 @@ class WorkKind < ActiveRecord::Base
 
   scope :usual, -> {where(other_flag: false).order(:display_order)}
   scope :by_type, -> (work_type){joins(:work_kind_types).where("work_kind_types.work_type_id = ?", work_type.genre_id).order("work_kinds.other_flag, work_kinds.display_order, work_kinds.id")}
-  
+
   def price
-    return term_price(Organization.term)
+    term_price(Organization.term)
   end
-  
+
   def price=(val)
     @p_price = val.to_i
   end
-  
+
   def term_price(term)
-    Rails.cache.fetch("work_kind_price_#{self.id}_#{term}", expires_in: 1.hour) do
+    cache_key = get_cache_key(term)
+    price_value = 0
+    if Rails.cache.exist?(cache_key)
+      price_value = Rails.cache.read(cache_key)
+    else
       work_kind_price = WorkKindPrice.by_term(self, term).first
-      return work_kind_price ? work_kind_price.price : 0
+      price_value = work_kind_price ? work_kind_price.price : 0
+      Rails.cache.write(cache_key, price_value)
     end
+    return price_value
   end
-  
+
   private
+
   def save_price
     term = Organization.term
-    work_kind_price = WorkKindPrice.where(work_kind_id: self.id, term: term).order(:id).first
+    work_kind_price = WorkKindPrice.where(work_kind_id: id, term: term).order(:id).first
     if work_kind_price
       work_kind_price.update_attributes(price: @p_price)
     else
-      WorkKindPrice.create(work_kind_id: self.id, term: term, price: @p_price)
+      WorkKindPrice.create(work_kind_id: id, term: term, price: @p_price)
     end
-    Rails.cache.write("work_kind_price_#{self.id}_#{term}", @p_price, expires_in: 1.hour)
+    Rails.cache.write(get_cache_key(term), @p_price, expires_in: 1.hour)
+  end
+
+  def get_cache_key(term)
+    "work_kind_price_#{id}_#{term}"
   end
 end
