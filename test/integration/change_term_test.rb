c66b046807acc41cf5be01f8e448fed9b574856e
Hiroyuki Adachi <adac@mtj.biglobe.ne.jp>
2017-02-03 05:35:14 -0800

    結合テストを少しだけ記述

---
+++ test/integration/change_term_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class ChangeTermTest < ActionDispatch::IntegrationTest
+  setup do
+    @organization = Organization.first
+    @system = System.find_by(term: @organization.term)
+  end
+
+  test "対象年度変更(実行:新規)" do
+    new_term = systems(:s2015).term + 1
+    patch_via_redirect(menu_path(@system), {system: {term: new_term}})
+    assert_response :success
+
+    get_via_redirect(root_path)
+    assert_response :success
+    assert_equal new_term, assigns(:system).term
+  end
+end
