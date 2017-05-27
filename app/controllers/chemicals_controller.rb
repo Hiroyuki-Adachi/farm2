16ef4bf5f4689e6887a77f9779c38cf9e48c4731
Hiroyuki Adachi <adac@mtj.biglobe.ne.jp>
2017-05-27 20:30:38 +0900

    薬剤マスタ保守修正

--- app/controllers/chemicals_controller.rb
+++ app/controllers/chemicals_controller.rb
@@ -3,7 +3,7 @@ class ChemicalsController < ApplicationController
   before_action :set_chemical_types, only: [:new, :create, :edit, :update]
 
   def index
-    @chemicals = ChemicalDecorator.decorate_collection(Chemical.list.page(params[:page]))
+    @chemicals = ChemicalDecorator.decorate_collection(Chemical.list)
   end
 
   def new
@@ -12,7 +12,7 @@ class ChemicalsController < ApplicationController
 
   def edit
   end
-  
+
   def create
     @chemical = Chemical.new(chemical_params)
     if @chemical.save
@@ -21,7 +21,7 @@ class ChemicalsController < ApplicationController
       render action: :new
     end
   end
-  
+
   def update
     if @chemical.update(chemical_params)
       redirect_to chemicals_path
@@ -29,23 +29,24 @@ class ChemicalsController < ApplicationController
       render action: :edit
     end
   end
-  
+
   def destroy
     @chemical.destroy
     redirect_to chemicals_path
   end
-  
+
   private
+
   def set_chemical
     @chemical = Chemical.find(params[:id])
     @chemical.term = @term
   end
-  
+
   def set_chemical_types
     @chemical_types = ChemicalType.all.order(:display_order)
   end
-  
+
   def chemical_params
-    return params.require(:chemical).permit(:name, :display_order, :chemical_type_id, :this_term_flag, :unit).merge(term: @term)
+    params.require(:chemical).permit(:name, :display_order, :chemical_type_id, :this_term_flag, :unit).merge(term: @term)
   end
 end
