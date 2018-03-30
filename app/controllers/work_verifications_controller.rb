c8fd3fa7da08197f6537dfabe0af6a4dad5c2bb9
Hiroyuki Adachi <adac@mtj.biglobe.ne.jp>
2018-03-30 20:58:14 +0900

    リファクタリング #2

--- app/controllers/work_verifications_controller.rb
+++ app/controllers/work_verifications_controller.rb
@@ -8,14 +8,17 @@ class WorkVerificationsController < ApplicationController
 
   def create
     WorkVerification.regist(Work.find(params[:work_id]), current_user.worker)
-    @works = WorkDecorator.decorate_collection(Work.for_verifications(@term, current_user.worker))
-    respond_to do |format|
-      format.html { render partial: "list" }
-    end
+    reload
   end
 
   def destroy
     WorkVerification.where(work_id: params[:work_id], worker_id: current_user.worker.id).destroy_all
+    reload
+  end
+
+  private
+
+  def reload
     @works = WorkDecorator.decorate_collection(Work.for_verifications(@term, current_user.worker))
     respond_to do |format|
       format.html { render partial: "list" }
