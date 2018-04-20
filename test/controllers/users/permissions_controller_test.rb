88192e3b84c9dd87908556e7b453919e43c3932e
Hiroyuki Adachi <adac@mtj.biglobe.ne.jp>
2018-04-21 02:57:50 +0900

    #32 権限保守のテスト

--- test/controllers/users/permissions_controller_test.rb
+++ test/controllers/users/permissions_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class Users::PermissionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    setup_ip
+    @user = users(:users1)
+  end
+
+  test "権限変更(表示)" do
+    get :new, user_id: @user.id
+    assert_response :success
+  end
+
+  test "権限変更(実行)" do
+    patch :create, user_id: @user, user: { permission_id: Permission::MANAGER.id }
+    assert_redirected_to users_path
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,18 +4,43 @@ class UsersControllerTest < ActionController::TestCase
   setup do
     setup_ip
     @user = users(:users1)
+    @create = { login_name: "testuser", password: "1111", password_confirmation: "1111" }
   end
 
-  test "パスワード変更(表示)" do
+  test "ユーザ一覧" do
+    get :index
+    assert_response :success
+  end
+
+  test "ユーザ新規作成(表示)" do
+    get :new, worker_id: workers(:worker5).id
+    assert_response :success
+  end
+
+  test "ユーザ新規作成(実行)" do
+    assert_difference('User.count') do
+      post :create, user: @create
+    end
+    assert_redirected_to users_path
+  end
+
+  test "ユーザ変更(表示)" do
     get :edit, id: @user
     assert_response :success
   end
 
-  test "パスワード変更(実行)" do
+  test "ユーザ変更(実行)" do
     patch :update, id: @user, user: { login_name: @user.login_name, password: "AAAA", password_confirmation: "AAA" }
     assert_response :success
 
     patch :update, id: @user, user: { login_name: @user.login_name, password: "AAAA", password_confirmation: "AAAA" }
     assert_redirected_to menu_index_path
+  end
+
+  test "ユーザ削除" do
+    assert_difference('User.count', -1) do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to users_path
   end
 end
