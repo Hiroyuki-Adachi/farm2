f76ec1d4893bf9911cb618866deb4e813fc48c78
Hiroyuki Adachi <adac@mtj.biglobe.ne.jp>
2018-04-26 21:49:14 +0900

    #41

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -58,3 +58,33 @@ Loading イメージ削除関数
 function removeLoading(){
     $("#loading").remove();
 }
+
+// bootbox-rails
+$.rails.allowAction = function(element) {
+    var message = element.attr('data-confirm');
+    if (!message) { return true; }
+  
+    var opts = {
+      message: message,
+      buttons: {
+          confirm: {
+              label: 'はい',
+              className: 'btn-success'
+          },
+          cancel: {
+              label: 'いいえ',
+              className: 'btn-danger'
+          }
+      },
+      callback: function(result) {
+        if (result) {
+          element.removeAttr('data-confirm');
+          element.trigger('click.rails')
+        }
+      }
+    };
+  
+    bootbox.confirm(opts);
+  
+    return false;
+  }
