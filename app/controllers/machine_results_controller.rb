e61a090c8a7c156155686ba2400d5813a11daf26
Hiroyuki Adachi <adac@mtj.biglobe.ne.jp>
2018-04-26 21:41:06 +0900

    #14

--- app/controllers/machine_results_controller.rb
+++ app/controllers/machine_results_controller.rb
@@ -7,6 +7,7 @@ class MachineResultsController < ApplicationController
     else
       @results = MachineResult.by_home(@term).to_a.uniq { |result| [result.work.id, result.machine_id] }
     end
+    @owner_totals = calc_totals(@results)
 
     respond_to do |format|
       format.html do
@@ -16,5 +17,24 @@ class MachineResultsController < ApplicationController
         render :content_type => 'text/csv; charset=cp943'
       end
     end
+  end
+
+  private
+
+  def calc_totals(results)
+    owner_totals = {}
+    results.each do |result|
+      owner_totals = set_totals(owner_totals, result, result.owner.id)
+    end
+
+    return owner_totals
+  end
+
+  def set_totals(totals, result, key)
+    totals[key] = {
+      count: (totals[key] ? totals[key][:count] : 0) + 1,
+      amount: (totals[key] ? totals[key][:amount] : 0) + result.amount
+    }
+    return totals
   end
 end
--- app/controllers/work_results_controller.rb
+++ app/controllers/work_results_controller.rb
@@ -7,6 +7,7 @@ class WorkResultsController < ApplicationController
     else
       @results = WorkResult.by_home(@term)
     end
+    @home_totals, @worker_totals = calc_totals(@results)
 
     respond_to do |format|
       format.html do
@@ -16,5 +17,27 @@ class WorkResultsController < ApplicationController
         render :content_type => 'text/csv; charset=cp943'
       end
     end
+  end
+
+  private
+
+  def calc_totals(results)
+    home_totals = {}
+    worker_totals = {}
+    results.each do |result|
+      home_totals = set_totals(home_totals, result, result.worker.home_id)
+      worker_totals = set_totals(worker_totals, result, result.worker.id)
+    end
+
+    return home_totals, worker_totals
+  end
+
+  def set_totals(totals, result, key)
+    totals[key] = {
+      count: (totals[key] ? totals[key][:count] : 0) + 1,
+      hours: (totals[key] ? totals[key][:hours] : 0) + result.hours,
+      amount: (totals[key] ? totals[key][:amount] : 0) + result.amount
+    }
+    return totals
   end
 end
