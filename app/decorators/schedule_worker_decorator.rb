c992aa80b6d21f18137612252b1ee68ef138014c
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2018-05-23 22:25:30 +0900

    #29 時刻を日本語で表示

--- app/decorators/schedule_worker_decorator.rb
+++ app/decorators/schedule_worker_decorator.rb
@@ -16,7 +16,7 @@ class ScheduleWorkerDecorator < Draper::Decorator
                 (model.schedule.worked_at - Date.today).to_i.to_s + "日後"
               end
     result += "(#{model.schedule.worked_at.strftime('%m月%d日')})(#{I18n.t('date.abbr_day_names')[model.schedule.worked_at.wday]})"
-    result += " #{schedule.start_at}"
+    result += " #{schedule.start_at_jp}"
     return result
   end
 
