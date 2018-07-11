5d888a5c0ee67a46967c423e2dc0c858e71b972a
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2018-07-11 23:17:21 +0900

    #48 原価計算テーブル

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,27 +2,27 @@
 #
 # Table name: organizations # 組織(体系)マスタ
 #
-#  id                    :integer          not null, primary key        # 組織(体系)マスタ
-#  name                  :string(20)       not null                     # 組織名称
-#  workers_count         :integer          default(12), not null        # 作業日報の作業者数
-#  lands_count           :integer          default(17), not null        # 作業日報の土地数
-#  machines_count        :integer          default(8), not null         # 作業日報の機械数
-#  chemicals_count       :integer          default(4), not null         # 作業日報の薬剤数
-#  daily_worker          :integer          default(0), not null         # 作業日報の作業者名付加情報
-#  consignor_code        :string(10)                                    # 委託者コード
-#  consignor_name        :string(40)                                    # 委託者コード
-#  bank_code             :string(4)        default("0000"), not null    # 口座の金融機関コード
-#  branch_code           :string(3)        default("000"), not null     # 口座の支店コード
-#  account_type_id       :integer          default(0), not null         # 口座種別
-#  account_number        :string(7)        default("0000000"), not null # 口座番号
-#  term                  :integer          default(0), not null         # 現在の年度(期)
+#  id                    :integer          not null, primary key         # 組織(体系)マスタ
+#  name                  :string(20)       not null                      # 組織名称
+#  workers_count         :integer          default(12), not null         # 作業日報の作業者数
+#  lands_count           :integer          default(17), not null         # 作業日報の土地数
+#  machines_count        :integer          default(8), not null          # 作業日報の機械数
+#  chemicals_count       :integer          default(4), not null          # 作業日報の薬剤数
+#  daily_worker          :integer          default("no_print"), not null # 作業日報の作業者名付加情報
+#  consignor_code        :string(10)                                     # 委託者コード
+#  consignor_name        :string(40)                                     # 委託者コード
+#  bank_code             :string(4)        default("0000"), not null     # 口座の金融機関コード
+#  branch_code           :string(3)        default("000"), not null      # 口座の支店コード
+#  account_type_id       :integer          default(0), not null          # 口座種別
+#  account_number        :string(7)        default("0000000"), not null  # 口座番号
+#  term                  :integer          default(0), not null          # 現在の年度(期)
 #  created_at            :datetime
 #  updated_at            :datetime
-#  url                   :string                                        # URL
-#  broccoli_work_type_id :integer                                       # ブロッコリ作業分類
-#  broccoli_work_kind_id :integer                                       # ブロッコリ種別分類
-#  chemical_group_count  :integer          default(1)                   # 薬剤グループ数
-#  rice_planting_id      :integer                                       # 田植作業種別
+#  url                   :string                                         # URL
+#  broccoli_work_type_id :integer                                        # ブロッコリ作業分類
+#  broccoli_work_kind_id :integer                                        # ブロッコリ種別分類
+#  chemical_group_count  :integer          default(1)                    # 薬剤グループ数
+#  rice_planting_id      :integer                                        # 田植作業種別
 #
 
 class Organization < ApplicationRecord
---
+++ app/models/total_cost_detail.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: total_cost_details # 集計原価(明細)
+#
+#  id            :bigint(8)        not null, primary key
+#  total_cost_id :integer          not null               # 集計原価
+#  work_type_id  :integer          not null               # 作業分類
+#  rate          :decimal(5, 2)    default(0.0), not null # 割合
+#  area          :decimal(7, 2)    not null               # 面積(α)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
+class TotalCostDetail < ApplicationRecord
+  belongs_to :total_cost
+  belongs_to :work_type
+end
