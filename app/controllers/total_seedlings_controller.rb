fbff815b7357ab510a4f28c2552ef5bdb62c8ce2
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2018-07-18 23:30:35 +0900

    #75 fixed

---
+++ app/controllers/total_seedlings_controller.rb
@@ -0,0 +1,18 @@
+class TotalSeedlingsController < ApplicationController
+  include TotalSeedlingsHelper
+  include PermitManager
+
+  def index
+    @seedling_price = current_system.seedling_price
+    @seedling_homes = SeedlingHome.usual(current_term)
+    @seedling_result_quantities = SeedlingResult.total(@seedling_homes)
+
+    respond_to do |format|
+      format.html do
+      end
+      format.csv do
+        render :content_type => 'text/csv; charset=cp943'
+      end
+    end
+  end
+end
