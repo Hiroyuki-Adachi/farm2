ac3cbe468fb9ae06da9c89fa8db463b19ef40076
Hiroyuki Adachi <jh-adachi@wako-grp.com>
2018-08-09 14:29:46 +0900

    #16 単価／消費税率登録

--- app/decorators/whole_crop_decorator.rb
+++ app/decorators/whole_crop_decorator.rb
@@ -1,4 +1,20 @@
 class WholeCropDecorator < Draper::Decorator
   delegate_all
   decorates_association :work
+
+  def price
+    model.price.to_i.to_s(:delimited)
+  end
+
+  def roll_price
+    model.roll_price.to_i.to_s(:delimited)
+  end
+
+  def tax_amount
+    model.tax_amount.to_i.to_s(:delimited)
+  end
+
+  def amount
+    model.amount.to_i.to_s(:delimited)
+  end
 end
