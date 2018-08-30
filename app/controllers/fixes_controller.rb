8e3f04c7dd2471d5d945c56e5171e14804cc76ea
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2018-08-30 23:26:01 +0900

    #80 前期集計

--- app/controllers/fixes_controller.rb
+++ app/controllers/fixes_controller.rb
@@ -3,26 +3,29 @@ class FixesController < ApplicationController
   before_action :set_fix, only: [:destroy]
   include PermitManager
 
-  MAX_WORKER = 3
+  MAX_WORKERS = 3
+  MAX_SEEDLINGS = 3
 
   def index
-    @fixes = FixDecorator.decorate_collection(Fix.usual(@term))
+    @fixes = FixDecorator.decorate_collection(Fix.usual(current_term))
   end
 
   def show
     @homes = Home.includes(:holder).for_finance1
     @lands = Land.for_finance1.group(:manager_id).sum(:area)
-    @work_hours, @work_amounts = WorkResult.by_works(@term, @fixed_at)
+    @work_hours = WorkResult.by_works(current_term, @fixed_at)
+    @seedling_homes = SeedlingHome.usual(current_term)
+    @machines = MachineResult.for_fix(current_term, @fixed_at).group(:home_id).sum(:fixed_amount)
     render action: :show1
   end
 
   def new
-    @works = WorkDecorator.decorate_collection(Work.no_fixed(@term))
-    @terms = WorkDecorator.get_terms(@term)
+    @works = WorkDecorator.decorate_collection(Work.no_fixed(current_term))
+    @terms = WorkDecorator.get_terms(current_term)
   end
 
   def create
-    Fix.do_fix(@term, @fixed_at, current_user.worker_id, params[:fixed_works])
+    Fix.do_fix(current_term, @fixed_at, current_user.worker_id, params[:fixed_works])
     redirect_to fixes_path
   end
 
