f686872241c83a205d770b6edb021fcc09d43b9a
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2019-04-23 21:42:25 +0900

    #100 test作成

--- test/controllers/personal_informations/lands_controller_test.rb
+++ test/controllers/personal_informations/lands_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
-class PersonalInformations::LandsControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get personal_informations_lands_show_url
-    assert_response :success
+class PersonalInformations::LandsControllerTest < ActionController::TestCase
+  setup do
+    @worker = workers(:worker1)
   end
 
+  test "個人情報(土地)" do
+    session[:user_id] = nil
+    get :index, params: {personal_information_token: @worker.token}
+    assert_response :success
+  end
 end
