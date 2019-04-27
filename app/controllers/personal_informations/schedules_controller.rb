c45f5690acc3545512cb0a3395dd3428c4f22ace
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2019-04-27 10:44:01 +0900

    #100 連絡事項など

--- app/controllers/personal_informations/schedules_controller.rb
+++ app/controllers/personal_informations/schedules_controller.rb
@@ -4,5 +4,6 @@ class PersonalInformations::SchedulesController < PersonalInformationsController
 
     @schedules = ScheduleWorkerDecorator.decorate_collection(ScheduleWorker.for_personal(@worker, SCHEDULE_DAY))
     @minute = Minute.for_personal(@worker).last&.decorate
+    @company = Worker.company.first
   end
 end
