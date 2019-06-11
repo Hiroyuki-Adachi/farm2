1ff190df23242b88cea677913e191183d39b6bd6
博之 足立 <Hiroyuki-Adachi@users.noreply.github.com>
2019-06-11 22:25:57 +0900

    #105 DBアクセス部

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -2,5 +2,6 @@ class ContractsController < ApplicationController
   include PermitManager
 
   def index
+    @works = Work.usual(current_term).exists_lands.where(work_type_id: current_organization.contract_work_type_id)
   end
 end
