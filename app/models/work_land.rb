766fa8a7c20ff32223d2f78e6752a5f6d660bf7d
博之 足立 <Hiroyuki-Adachi@users.noreply.github.com>
2019-06-12 21:43:14 +0900

    #105 支払額計算処理

--- app/models/work_land.rb
+++ app/models/work_land.rb
@@ -24,4 +24,21 @@ class WorkLand < ApplicationRecord
       .where("lands.manager_id = ?", home.id)
       .order("lands.display_order, lands.id, works.worked_at")
   }
+
+  def interim_cost
+    (work.sum_workers_amount * land.area / work.sum_areas).round
+  end
+
+  def largest?
+    max_area = work.lands.map(&:area).max
+    return false if land.area != max_area
+
+    return land.id == work.lands.select { |land| land.area == max_area}.map(&:id).min
+  end
+
+  def cost
+    local_cost = interim_cost
+    return local_cost unless largest?
+    return local_cost + (work.sum_workers_amount - work.work_lands.map(&:interim_cost).sum)
+  end
 end
