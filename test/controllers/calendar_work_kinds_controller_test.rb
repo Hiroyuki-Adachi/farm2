cd0ec7d15d9d74a1349d22e69afd59acd3e95e21
博之 足立 <Hiroyuki-Adachi@users.noreply.github.com>
2019-07-07 08:30:33 +0900

    #52 #107

--- test/controllers/calendar_work_kinds_controller_test.rb
+++ test/controllers/calendar_work_kinds_controller_test.rb
@@ -3,6 +3,9 @@ require 'test_helper'
 class CalendarWorkKindsControllerTest < ActionController::TestCase
   setup do
     setup_ip
+    @calendar_term = 2014
+    @work_kind = work_kinds(:work_kind_taue)
+    @text_color = "#123456"
   end
 
   test "カレンダー制御" do
@@ -14,5 +17,15 @@ class CalendarWorkKindsControllerTest < ActionController::TestCase
     session[:user_id] = users(:user_checker).id
     get :index
     assert_response :error
+  end
+
+  test "カレンダー制御(登録)" do
+    assert_difference('CalendarWorkKind.count') do
+      post :create, params: {calendar_term: @calendar_term,
+                             work_kind_id: [@work_kind.id],
+                             text_color: {@work_kind.id => @text_color}}
+    end
+
+    assert_redirected_to calendars_path
   end
 end
