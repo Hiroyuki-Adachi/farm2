402196280152f176b5531531f575df46a84150e4
博之 足立 <Hiroyuki-Adachi@users.noreply.github.com>
2019-07-06 22:19:29 +0900

    #107 印刷

--- app/models/calendar_work_kind.rb
+++ app/models/calendar_work_kind.rb
@@ -18,15 +18,17 @@ class CalendarWorkKind < ApplicationRecord
 
   def self.regist(user_id, params)
     work_kinds = []
-    if params[:work_kind_id]
-      params[:work_kind_id].each do |work_kind_id|
-        calendar_work_kind = CalendarWorkKind.find_by(user_id: user_id, work_kind_id: work_kind_id)
-        work_kinds << work_kind_id.to_i
-        if calendar_work_kind
-          calendar_work_kind.update(text_color: params[:text_color][work_kind_id]) if calendar_work_kind.text_color != params[:text_color][work_kind_id]
-        else
-          CalendarWorkKind.create(user_id: user_id, work_kind_id: work_kind_id, text_color: params[:text_color][work_kind_id])
-        end
+    unless params[:work_kind_id]
+      CalendarWorkKind.where(user_id: user_id).find_each(&:destroy)
+      return
+    end
+    params[:work_kind_id].each do |work_kind_id|
+      calendar_work_kind = CalendarWorkKind.find_by(user_id: user_id, work_kind_id: work_kind_id)
+      work_kinds << work_kind_id.to_i
+      if calendar_work_kind
+        calendar_work_kind.update(text_color: params[:text_color][work_kind_id]) if calendar_work_kind.text_color != params[:text_color][work_kind_id]
+      else
+        CalendarWorkKind.create(user_id: user_id, work_kind_id: work_kind_id, text_color: params[:text_color][work_kind_id])
       end
     end
     CalendarWorkKind.where(user_id: user_id).where.not(work_kind_id: work_kinds).find_each(&:destroy)
