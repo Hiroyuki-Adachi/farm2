5cde1cac28a9c995750af1a9e82af0050b95bf9f
博之 足立 <Hiroyuki-Adachi@users.noreply.github.com>
2019-07-08 20:52:12 +0900

    #107 理事会対応

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -4,6 +4,8 @@ class CalendarsController < ApplicationController
   def index
     @year = current_user.calendar_term
     @calendar_work_kinds = CalendarWorkKind.usual(current_user).to_a
-    @works = Work.for_calendar(@year, @calendar_work_kinds.map(&:work_kind_id))
+    @work_kind_ids = @calendar_work_kinds.map(&:work_kind_id)
+    @schedules = Schedule.for_calendar(@year, @work_kind_ids)
+    @works = Work.for_calendar(@year, @work_kind_ids - @schedules.map(&:work_kind_id))
   end
 end
