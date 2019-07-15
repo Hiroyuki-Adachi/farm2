0e06163b92c484caedf4ed6dd6b4d6400a1bd180
博之 足立 <Hiroyuki-Adachi@users.noreply.github.com>
2019-07-15 22:49:27 +0900

    #72 モデル定義

---
+++ app/models/drying_moth.rb
@@ -0,0 +1,2 @@
+class DryingMoth < ApplicationRecord
+end
---
+++ db/migrate/20190715011355_create_dryings.rb
@@ -0,0 +1,21 @@
+class CreateDryings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :dryings, comment: "乾燥" do |t|
+      t.integer :work_id,        {null: false, default: 0, comment: "稲刈作業"}
+      t.integer :work_type_id,   {null: true, comment: "作業分類"}
+      t.integer :home_id,        {null: false, default: 0, comment: "担当世帯"}
+      t.integer :drying_type_id, {null: false, default: 0, comment: "乾燥種別"}
+      t.date    :shipped_on,     {null: true, comment: "出荷日"}
+      t.decimal :water_content,  {null: true, scale: 1, precision: 3, comment: "水分"}
+      t.decimal :rice_weight,    {null: false, default: 0, scale: 1, precision: 5, comment: "乾燥米(kg)"}
+      t.decimal :fixed_amount,   {null: false, default: 0, scale: 0, precision: 7, comment: "確定額"}
+
+      t.timestamps
+    end
+    add_column :organizations, :harvesting_work_kind_id, :integer, {null: true, comment: "稲刈作業種別"}
+    add_column :homes, :drying_order, :integer, {null: false, default: 0, comment: "出力順(乾燥調整用)"}
+    add_column :systems, :dry_price, :decimal,    {null: false, default: 0, scale: 0, precision: 4, comment: "基準額(乾燥のみ)"}
+    add_column :systems, :adjust_price, :decimal, {null: false, default: 0, scale: 0, precision: 4, comment: "基準額(調整のみ)"}
+    add_column :systems, :dry_adjust_price, :decimal, {null: false, default: 0, scale: 0, precision: 4, comment: "基準額(乾燥調整)"}
+  end
+end
