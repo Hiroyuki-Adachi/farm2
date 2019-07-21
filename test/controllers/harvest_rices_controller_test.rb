aea3328d9cb7e5395fe2f35b2cc8f87a5936ec3f
博之 足立 <Hiroyuki-Adachi@users.noreply.github.com>
2019-07-21 20:19:14 +0900

    #72 #52 乾燥調整(スマホ)テスト

--- test/controllers/harvest_rices_controller_test.rb
+++ test/controllers/harvest_rices_controller_test.rb
@@ -1,4 +1,18 @@
 require 'test_helper'
 
-class HarvestRicesControllerTest < ActionDispatch::IntegrationTest
+class HarvestRicesControllerTest < ActionController::TestCase
+  setup do
+    setup_ip
+  end
+
+  test "収穫一覧(水稲)" do
+    get :index
+    assert_response :success
+  end
+
+  test "収穫一覧(水稲)(検証者以外)" do
+    session[:user_id] = users(:user_checker).id
+    get :index
+    assert_response :error
+  end
 end
