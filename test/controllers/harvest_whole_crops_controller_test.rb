2f67e26636aa8b1440aaf1c82459836147ea4341
博之 足立 <Hiroyuki-Adachi@users.noreply.github.com>
2019-07-21 20:19:14 +0900

    #72 #52 乾燥調整(スマホ)テスト

--- test/controllers/harvest_whole_crops_controller_test.rb
+++ test/controllers/harvest_whole_crops_controller_test.rb
@@ -1,4 +1,4 @@
 require 'test_helper'
 
-class HarvestWholeCropsControllerTest < ActionDispatch::IntegrationTest
+class HarvestWholeCropsControllerTest < ActionController::TestCase
 end
--- test/controllers/personal_informations/dryings_controller_test.rb
+++ test/controllers/personal_informations/dryings_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
-class PersonalInformations::DryingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get personal_informations_dryings_index_url
-    assert_response :success
+class PersonalInformations::DryingsControllerTest < ActionController::TestCase
+  setup do
+    @worker = workers(:worker31)
   end
 
+  test "個人情報(乾燥調整)" do
+    session[:user_id] = nil
+    get :index, params: {personal_information_token: @worker.token}
+    assert_response :success
+  end
 end
