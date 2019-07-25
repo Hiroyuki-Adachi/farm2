f2aaf100a3d8be5fc794f51c5d3bc0d60906d988
博之 足立 <Hiroyuki-Adachi@users.noreply.github.com>
2019-07-25 22:44:08 +0900

    #72 会計処理

--- app/controllers/fixes_controller.rb
+++ app/controllers/fixes_controller.rb
@@ -19,9 +19,12 @@ class FixesController < ApplicationController
     when SHOW1_MONTH
       @lands = Land.for_finance1.group(:manager_id).sum(:area)
       @seedling_homes = SeedlingHome.usual(current_term)
-      @contracts = WorkLand.for_fix(current_term, @fixed_at, current_organization.contract_work_type_id)
-                           .group("lands.manager_id").sum("work_lands.fixed_cost")
       render action: :show1
+    when SHOW2_MONTH
+      @lands1 = Land.for_finance1.group(:manager_id).sum(:area)
+      @lands2 = Land.for_finance2.group(:manager_id).sum(:area)
+      set_dryings
+      render action: :show2
     else
       render action: :show
     end
@@ -56,5 +59,17 @@ class FixesController < ApplicationController
     @homes = Home.includes(:workers).for_finance1
     @work_hours = WorkResult.by_works(current_term, @fixed_at)
     @machines = MachineResult.for_fix(current_term, @fixed_at).group(:home_id).sum(:fixed_amount)
+    @contracts = WorkLand.for_fix(current_term, @fixed_at, current_organization.contract_work_type_id)
+                  .group("lands.manager_id").sum("work_lands.fixed_cost")
+  end
+
+  def set_dryings
+    @total_dryings = {}
+    @waste_totals = {}
+    Home.for_drying.each do |home|
+      @total_dryings[home.id], @waste_totals[home.id] = Drying.calc_total(
+        Drying.by_home(current_term, home), home
+      )
+    end
   end
 end
