fad7f78e9cde736e4ebd94012bea04515b1b82f9
Hiroyuki Adachi <jh-adachi@wako-grp.com>
2019-07-25 13:13:24 +0900

    #72 #52

--- test/controllers/total_dryings_controller_test.rb
+++ test/controllers/total_dryings_controller_test.rb
@@ -1,9 +1,18 @@
 require 'test_helper'
 
-class TotalDryingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get total_dryings_index_url
+class TotalDryingsControllerTest < ActionController::TestCase
+  setup do
+    setup_ip
+  end
+
+  test "乾燥集計一覧" do
+    get :index
     assert_response :success
   end
 
+  test "乾燥集計(管理者以外)" do
+    session[:user_id] = users(:user_checker).id
+    get :index
+    assert_response :error
+  end
 end
