7a03f295e2d769d0f7eb51b7bff3a7fe8b8d8c2e
博之 足立 <Hiroyuki-Adachi@users.noreply.github.com>
2019-08-03 19:54:48 +0900

    #112 誤削除ファイル復活

---
+++ app/assets/javascripts/fixes.js
@@ -0,0 +1,36 @@
+function calc_total()
+{
+    var total_hours = 0.0;
+    var total_amount = 0;
+    var total_machine = 0;
+    var id;
+
+    $("input[name^='fixed_works']:checked").each(function(i, e) {
+        id = e.value;
+        total_hours += parseFloat($("#hours_" + id).html());
+        total_amount += parseInt($("#amount_" + id).html().replace(/,/g, ""));
+        total_machine += parseInt($("#machine_" + id).html().replace(/,/g, ""));
+    });
+
+    $("#total_hours").html(total_hours.toFixed(1));
+    $("#total_amount").html(insertComma(total_amount));
+    $("#total_machine").html(insertComma(total_machine));
+}
+
+function insertComma(num)
+{
+    var dest = num.toString();
+    var tmp;
+
+    while(dest != (tmp = dest.replace(/^([+-]?\d+)(\d{3})/, "$1,$2"))) {
+        dest = tmp;
+    }
+    return dest;
+}
+
+function checkAll(val)
+{
+    $("input[name^='fixed_works']").prop("checked", val);
+
+    calc_total();
+}
