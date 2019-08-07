fdbccc2615a51705986896f3dab262ccee69f29a
博之 足立 <Hiroyuki-Adachi@users.noreply.github.com>
2019-08-07 22:20:34 +0900

    #100 メニューをアイコン化

--- app/controllers/personal_informations/machines_controller.rb
+++ app/controllers/personal_informations/machines_controller.rb
@@ -2,6 +2,6 @@ class PersonalInformations::MachinesController < PersonalInformationsController
   def index
     to_error_path unless @worker
 
-    @machines = MachineResultDecorator.decorate_collection(MachineResult.for_personal(@worker.home, worked_from))
+    @machines = MachineResultDecorator.decorate_collection(MachineResult.for_personal(@worker.home, between_worked_at[0]))
   end
 end
