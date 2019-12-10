963966da3403d3dab394952e981cdf7b415b73d6
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2019-12-10 21:49:25 +0900

    #125

--- app/models/fix.rb
+++ app/models/fix.rb
@@ -30,27 +30,26 @@ class Fix < ApplicationRecord
     works_amount = 0
     works_count = 0
     machines_amount = 0
-    Fix.transaction do
-      Work.find(works_ids).each do |work|
-        work.work_results.each do |result|
-          amount = result.hours * work.work_kind.price
-          result.update(fixed_hours: result.hours, fixed_price: work.work_kind.price, fixed_amount: amount)
-          works_amount += amount
-          hours += result.hours
-        end
-        work.machine_results.to_a.uniq(&:machine_id).each do |result|
-          result.update(fixed_quantity: result.quantity, fixed_adjust_id: result.adjust.id, fixed_price: result.price, fixed_amount: result.amount)
-          machines_amount += result.amount
-        end
-        work.work_lands.each do |work_land|
-          work_land.update(fixed_cost: work_land.cost)
-        end
-        work.update(fixed_at: fixed_at)
-        works_count += 1
-      end
 
-      Fix.create(term: term, fixed_at: fixed_at, works_count: works_count, hours: hours, works_amount: works_amount, machines_amount: machines_amount, fixed_by: fixed_by)
+    Work.find(works_ids).each do |work|
+      work.work_results.each do |result|
+        amount = result.hours * work.work_kind.price
+        result.update(fixed_hours: result.hours, fixed_price: work.work_kind.price, fixed_amount: amount)
+        works_amount += amount
+        hours += result.hours
+      end
+      work.machine_results.to_a.uniq(&:machine_id).each do |result|
+        result.update(fixed_quantity: result.quantity, fixed_adjust_id: result.adjust.id, fixed_price: result.price, fixed_amount: result.amount)
+        machines_amount += result.amount
+      end
+      work.work_lands.each do |work_land|
+        work_land.update(fixed_cost: work_land.cost)
+      end
+      work.update(fixed_at: fixed_at)
+      works_count += 1
     end
+
+    Fix.create(term: term, fixed_at: fixed_at, works_count: works_count, hours: hours, works_amount: works_amount, machines_amount: machines_amount, fixed_by: fixed_by)
   end
 
   private
