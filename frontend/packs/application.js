151dbf76db28768f9c89bc57f5d40d33055317e1
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2020-04-20 23:01:04 +0900

    #132 autocomplete

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -7,11 +7,11 @@ import "chart.js"
 window.bootbox = require('bootbox');
 
 const $ = require('jquery');
+const images = require.context('../images', true);
 
 require('jquery-ui');
 require('jquery-ui/ui/widgets/sortable');
 require('jquery-ui/ui/widgets/autocomplete');
-
-const images = require.context('../images', true);
+require('jquery-ui/themes/base/all.css');
 
 import '../stylesheets/application';
