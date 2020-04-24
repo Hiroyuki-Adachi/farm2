9ecaa7edb4c3f9c080295534689d3d81c11755a7
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2020-04-24 19:54:33 +0900

    #132 jquery.selection

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery.selection
 //= require i18n
 //= require i18n/translations
 //= require dateformat
