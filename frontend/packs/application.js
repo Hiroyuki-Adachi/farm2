7ede9eeb6846fa6225bce202c660f9ee9fed5e9d
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2020-04-25 20:50:06 +0900

    #132 i18n

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,14 +1,17 @@
 require("@rails/ujs").start();
 window.Rails = require("@rails/ujs");
 
-import "bootstrap"
-import "chart.js"
+import "bootstrap";
+import "chart.js";
 
 window.bootbox = require('bootbox');
 
 const $ = require('jquery');
 const images = require.context('../images', true);
 window.moment = require('moment');
+
+import I18n from 'i18n-js';
+window.I18n = I18n;
 
 require('jquery-ui');
 require('jquery-ui/ui/widgets/sortable');
