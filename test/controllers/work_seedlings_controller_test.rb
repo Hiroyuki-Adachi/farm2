857b86e425474cf45c271c0bc0a728bbb9bc8a68
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2020-07-11 14:26:32 +0900

    #132

--- test/controllers/work_seedlings_controller_test.rb
+++ test/controllers/work_seedlings_controller_test.rb
@@ -1,12 +1,4 @@
 require 'test_helper'
 
 class WorkSeedlingsControllerTest < ActionController::TestCase
-  setup do
-    setup_ip
-  end
-
-  test "should get index" do
-    get work_seedlings_index_url
-    assert_response :success
-  end
 end
