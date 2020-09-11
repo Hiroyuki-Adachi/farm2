bfd6d0851d343fccb46cbade0cc2dc73d0ad23c9
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2020-09-11 23:27:44 +0900

    #152 #52 plan_work_type test

--- test/controllers/plans/work_types_controller_test.rb
+++ test/controllers/plans/work_types_controller_test.rb
@@ -1,5 +1,26 @@
 require 'test_helper'
 
-class Plans::WorkTypesControllerTest < ActionDispatch::IntegrationTest
+class Plans::WorkTypesControllerTest < ActionController::TestCase
+  setup do
+    setup_ip
+    session[:user_id] = users(:user_manager).id
+  end
 
+  test "育苗計画1(表示)" do
+    get :new
+    assert_response :success
+  end
+
+  test "育苗計画1(表示)(管理者以外)" do
+    session[:user_id] = users(:user_checker).id
+    get :new
+    assert_response :error
+  end
+
+  test "育苗計画1(作成)" do
+    work_type = work_types(:work_type_koshi)
+    assert_difference('PlanWorkType.count') do
+      post :create, params: {work_types: {work_type.id => {month: 4, area: 100}}}
+    end
+  end
 end
