deeaab20818b7035f1c06caacf7a50041aa6eb48
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2020-09-13 20:40:04 +0900

    #152 lands clear

--- app/controllers/plans/lands_controller.rb
+++ app/controllers/plans/lands_controller.rb
@@ -4,12 +4,17 @@ class Plans::LandsController < ApplicationController
   helper GmapHelper
 
   def new
-    @lands = Land.regionable.includes(:plan_land)
+    @lands = Land.regionable.includes(:plan_land, :owner)
     @work_types = WorkType.land
   end
 
   def create
     PlanLand.create_all(params["land"])
+    redirect_to new_plans_land_path
+  end
+
+  def destroy
+    PlanLand.clear_all(Date.today)
     redirect_to new_plans_land_path
   end
 
