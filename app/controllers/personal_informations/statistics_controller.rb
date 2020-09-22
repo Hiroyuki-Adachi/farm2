f2a08d12d3e7b1a214745a106fba05a06b7e17a5
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2020-09-22 16:53:20 +0900

    #60

--- app/controllers/personal_informations/statistics_controller.rb
+++ app/controllers/personal_informations/statistics_controller.rb
@@ -1,5 +1,7 @@
 class PersonalInformations::StatisticsController < PersonalInformationsController
   def index
     @total_all = Work.total_by_worker(@worker.id, current_term)
+    @total_month1 = Work.total_by_month(@worker.id, current_term - 1)
+    @total_month2 = Work.total_by_month(@worker.id, current_term)
   end
 end
