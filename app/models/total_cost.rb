38dc728e2ed6476233804d217f97ed20b1d152af
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2021-06-06 15:28:29 +0900

    #172 activeHash が悪そう

--- app/models/total_cost.rb
+++ app/models/total_cost.rb
@@ -34,7 +34,7 @@ class TotalCost < ApplicationRecord
   belongs_to :seedling_home, optional: true
   belongs_to :wcs_land, class_name: "WholeCropLand", foreign_key: "whole_crop_land_id", optional: true
   belongs_to :land, optional: true
-  belongs_to :total_cost_type
+  belongs_to_active_hash :total_cost_type
 
   has_many :total_cost_details, dependent: :destroy
 
