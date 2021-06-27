11fd7ae722edd2a1aaff322731bb984834d2f39d
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2021-06-27 16:29:30 +0900

    #48 土地原価 UPDATE

--- app/controllers/lands/fees_controller.rb
+++ app/controllers/lands/fees_controller.rb
@@ -8,4 +8,19 @@ class Lands::FeesController < ApplicationController
   def edit
     @home = Home.find(params[:id])
   end
+
+  def update
+    LandFee.upsert(params[:id], fee_params)
+    redirect_to(lands_fees_path)
+  end
+
+  def create
+    # TODO: 一括作成
+  end
+
+  private
+
+  def fee_params
+    params.permit(land_fees: [:manage_fee, :peasant_fee, :term, :id, :land_id])[:land_fees]
+  end
 end
