6762859c44c7ee8ac168930309e72510312e019d
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2021-07-03 22:45:39 +0900

    #189 結果表示

--- test/controllers/total_costs/machines_controller_test.rb
+++ test/controllers/total_costs/machines_controller_test.rb
@@ -1,4 +1,18 @@
 require "test_helper"
 
-class TotalCosts::MachinesControllerTest < ActionDispatch::IntegrationTest
+class TotalCosts::MachinesControllerTest < ActionController::TestCase
+  setup do
+    setup_ip
+  end
+
+  test "機械稼働一覧" do
+    get :index
+    assert_response :success
+  end
+
+  test "機械稼働一覧(管理者以外)" do
+    session[:user_id] = users(:user_checker).id
+    get :index
+    assert_response :error
+  end
 end
