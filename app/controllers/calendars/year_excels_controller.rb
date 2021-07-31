97cec4309539fd07bda3aba87d7a5f799326e375
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2021-07-31 16:09:54 +0900

    #191 カレンダー出力

--- app/controllers/calendars/year_excels_controller.rb
+++ app/controllers/calendars/year_excels_controller.rb
@@ -13,19 +13,13 @@ class Calendars::YearExcelsController < CalendarsController
     workbook.calc_pr.calc_on_save = true
     workbook.calc_pr.force_full_calc = true
 
-    calendar_work_kind = @calendar_work_kinds[0]
     data_sheet = workbook[0]
 
-    title_cell = data_sheet[3][0]
-    title_cell.change_contents("#{calendar_work_kind.work_kind.name}")
-
     data_sheet[5][0].change_contents(@works[0].model.worked_at.year)
-    first_month = @works[0].model.worked_at.month
-    data_sheet[5][2].change_contents(first_month)
 
     holiday_row = 3
     holiday_sheet = workbook[1]
-    (Date.new(@year, first_month, 1)..Date.new(@year, first_month + MAX_MONTHS - 1, 1)).each do |today|
+    (Date.new(@year, 1, 1)..Date.new(@year, 12, 31)).each do |today|
       if HolidayJp.holiday?(today)
         holiday_sheet[holiday_row][0].change_contents(today.month)
         holiday_sheet[holiday_row][1].change_contents(today.day)
@@ -36,9 +30,8 @@ class Calendars::YearExcelsController < CalendarsController
     end
 
     @works.each do |work|
-      break if work.model.worked_at.month - first_month >= MAX_MONTHS
-      work_cell = data_sheet[work.model.worked_at.day * 2 + 4][(work.model.worked_at.month - first_month) * 3 + 2]
-      work_cell.change_contents("#{work.exact_work_type_name}(#{work.sum_areas}a)")
+      work_cell = data_sheet[work.model.worked_at.day * 2 + 4][(work.model.worked_at.month - 1) * 3 + 2]
+      work_cell.change_contents("#{work.work_kind.name}(#{work.exact_work_type_name})")
     end
 
     respond_to do |format|
