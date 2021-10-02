389ae139a1e2e5b9f2955252bfc42ff827ca4b3f
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2021-10-02 14:34:26 +0900

    #198 日報

--- app/helpers/works_helper.rb
+++ app/helpers/works_helper.rb
@@ -2,4 +2,16 @@ module WorksHelper
   def updatable_work(user, work)
     (user.checkable? || work.created_by == user.worker.id) && work.term == user.term
   end
+
+  def chemical_per_area(work, chemical_quantity, chemical)
+    area_quantity = work.sum_areas.zero? ? 0 : (chemical_quantity / work.sum_areas * 10)
+    area_quantity *= chemical.base_quantity
+    if (area_quantity > 1000000)
+      return sprintf("%.2f", area_quantity / 1000000) + chemical.base_unit.mega_name
+    end
+    if (area_quantity > 1000)
+      return sprintf("%.2f", area_quantity / 1000) + chemical.base_unit.kilo_name
+    end
+    return sprintf("%.2f", area_quantity) + chemical.base_unit.name
+  end
 end
