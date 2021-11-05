e8fb7b1ef11b622bd1ffc0df927ba9ba117ca9d8
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2021-11-05 21:21:21 +0900

    #199 原価種別保守

--- app/models/cost_type.rb
+++ app/models/cost_type.rb
@@ -10,4 +10,23 @@
 #  updated_at                       :datetime         not null
 #
 class CostType < ApplicationRecord
+  has_many :work_kinds, dependent: :nullify
+
+  after_save :save_work_kinds
+
+  validates :name, presence: true
+  validates :phonetic, presence: true
+  validates :phonetic, format: { with: /\A[\p{Hiragana}ー－A-Z0-9]+\z/ }, if: proc { |x| x.phonetic.present?}
+  validates :display_order, presence: true
+
+  scope :usual, -> { order(display_order: :asc) }
+
+  def work_kind_ids=(value)
+    @work_kind_ids = value
+  end
+
+  def save_work_kinds
+    WorkKind.where(cost_type_id: self.id).update(cost_type_id: nil)
+    WorkKind.where(id: @work_kind_ids).update(cost_type_id: self.id)
+  end
 end
