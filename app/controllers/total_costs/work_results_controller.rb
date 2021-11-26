442d518c94e8171ed82b2069721d96fa096eaf7f
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2021-11-26 23:18:40 +0900

    #199 for error

--- app/controllers/total_costs/work_results_controller.rb
+++ app/controllers/total_costs/work_results_controller.rb
@@ -2,6 +2,10 @@ class TotalCosts::WorkResultsController < ApplicationController
   include PermitManager
 
   def index
-    @errors = TotalCost.for_worker(current_term).where(cost_type_id: nil)
+    errors = TotalCost.for_worker(current_term).where(cost_type_id: nil)
+    if errors.exists?
+      @work_kinds = WorkKind.where(id: errors.joins(:work).pluck("works.work_kind_id"))
+      render "errors"
+    end
   end
 end
