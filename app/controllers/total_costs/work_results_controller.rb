8e2629031a1b698aac6695cbd2a92a8293a71379
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2021-11-27 16:19:40 +0900

    #199 一応完成

--- app/controllers/total_costs/work_results_controller.rb
+++ app/controllers/total_costs/work_results_controller.rb
@@ -7,5 +7,14 @@ class TotalCosts::WorkResultsController < ApplicationController
       @work_kinds = WorkKind.where(id: errors.joins(:work).pluck("works.work_kind_id"))
       render "errors"
     end
+    @work_types = WorkType.cost
+    @cost_types = CostType.usual
+    @total_costs = TotalCost.sum_work_results(current_term)
+  end
+
+  private
+
+  def menu_name
+    return :total_costs
   end
 end
