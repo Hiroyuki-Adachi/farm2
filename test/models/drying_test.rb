ee5325991d0ded54f7b1fe8c961c77b9aab5a508
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2021-12-05 14:14:59 +0900

    #201 TEST header

--- test/models/drying_test.rb
+++ test/models/drying_test.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: dryings
+#
+#  id                       :bigint           not null, primary key
+#  carried_on(搬入日)       :date             not null
+#  fixed_amount(確定額)     :decimal(7, )
+#  shipped_on(出荷日)       :date
+#  term(年度(期))           :integer          not null
+#  water_content(水分)      :decimal(3, 1)
+#  created_at               :datetime         not null
+#  updated_at               :datetime         not null
+#  drying_type_id(乾燥種別) :integer          default(0), not null
+#  home_id(担当世帯)        :integer          default(0), not null
+#  work_type_id(作業分類)   :integer
+#
+# Indexes
+#
+#  dryings_secondary  (carried_on,home_id) UNIQUE
+#
 require 'test_helper'
 
 class DryingTest < ActiveSupport::TestCase
