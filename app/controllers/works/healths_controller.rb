370fedce78b4fb28d8cbd628086cafcc013e0bd9
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2021-12-09 22:01:03 +0900

    #201 routes 変更

--- app/controllers/works/healths_controller.rb
+++ app/controllers/works/healths_controller.rb
@@ -1,4 +1,11 @@
-class Works::HealthsController < ApplicationController
-  def edit
+class Works::HealthsController < WorksController
+  before_action :set_work, only: [:index]
+
+  def index
+    @results = @work.model.work_results
+  end
+
+  def menu_name
+    return :works
   end
 end
