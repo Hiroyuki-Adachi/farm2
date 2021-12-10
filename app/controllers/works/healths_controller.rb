46f1e49d2679e28b26275a8d484db4d46b15f834
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2021-12-10 21:03:19 +0900

    #201 変数追加

--- app/controllers/works/healths_controller.rb
+++ app/controllers/works/healths_controller.rb
@@ -1,8 +1,11 @@
 class Works::HealthsController < WorksController
-  before_action :set_work, only: [:index]
+  before_action :set_work, only: [:index, :create]
 
   def index
     @results = @work.model.work_results
+  end
+
+  def create
   end
 
   def menu_name
