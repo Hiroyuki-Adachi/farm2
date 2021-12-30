19eb9b360101290c7fa05a9ce18083ecd5d4bb03
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2021-12-30 17:03:37 +0900

    #196 在庫TEST

--- test/controllers/chemicals/stocks_controller_test.rb
+++ test/controllers/chemicals/stocks_controller_test.rb
@@ -1,4 +1,29 @@
 require "test_helper"
 
 class Chemicals::StocksControllerTest < ActionController::TestCase
+  setup do
+    setup_ip
+    @chemical_term = chemical_terms(:chemical_term_3_2015)
+  end
+
+  test "農薬在庫一覧" do
+    get :index
+    assert_response :success
+  end
+
+  test "農薬在庫一覧(管理者以外)" do
+    session[:user_id] = users(:user_checker).id
+    get :index
+    assert_response :error
+  end
+
+  test "農薬在庫(AJAX)" do
+    get :load, params: {term: 2015, chemical_type_id: 3}
+    assert_response :success
+  end
+
+  test "農薬在庫検索" do
+    get :search, params: {chemical_id: @chemical_term.id}
+    assert_response :success
+  end
 end
