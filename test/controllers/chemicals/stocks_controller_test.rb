94c9a443d920d182241224c620294819e64cb23f
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2021-12-31 12:43:32 +0900

    #196 在庫 TEST

--- test/controllers/chemicals/stocks_controller_test.rb
+++ test/controllers/chemicals/stocks_controller_test.rb
@@ -4,6 +4,7 @@ class Chemicals::StocksControllerTest < ActionController::TestCase
   setup do
     setup_ip
     @chemical_term = chemical_terms(:chemical_term_3_2015)
+    @stock = chemical_stocks(:stock_edit)
   end
 
   test "農薬在庫一覧" do
@@ -24,6 +25,53 @@ class Chemicals::StocksControllerTest < ActionController::TestCase
 
   test "農薬在庫検索" do
     get :search, params: {chemical_id: @chemical_term.id}
+    assert_response :success
+  end
+
+  test "農薬在庫新規作成(表示)" do
+    get :new, params: {chemical_id: @chemical_term.id}
+    assert_response :success
+  end
+
+  test "農薬在庫新規作成(実行)" do
+    assert_difference('ChemicalStock.count') do
+      post :create, params: {
+        chemical_id: @chemical_term.id, 
+        chemical_stock: {
+          stock_on: '2015-03-03',
+          name: 'TEST',
+          stored: 20
+      }}
+    end
+    assert_response :success
+  end
+
+  test "農薬在庫編集(表示)" do
+    get :edit, params: {chemical_id: @chemical_term.id, id: @stock.id}
+    assert_response :success
+  end
+
+  test "農薬在庫編集(実行)" do
+    assert_no_difference('ChemicalStock.count') do
+      post :update, params: {
+        chemical_id: @chemical_term.id, 
+        id: @stock.id,
+        chemical_stock: {
+          stock_on: '2015-12-31',
+          name: 'UPDATE',
+          stored: 1
+      }}
+    end
+    assert_response :success
+  end
+
+  test "農薬在庫削除(実行)" do
+    assert_difference('ChemicalStock.count', -1) do
+      delete :destroy, params: {
+        chemical_id: @chemical_term.id, 
+        id: @stock.id
+      }
+    end
     assert_response :success
   end
 end
