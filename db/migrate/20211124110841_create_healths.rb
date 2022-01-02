fcb761d1afd2d6403b2da112c99da4b0e79fff34
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2022-01-02 12:21:40 +0900

    #172 for Ruby3 migration

--- db/migrate/20211124110841_create_healths.rb
+++ db/migrate/20211124110841_create_healths.rb
@@ -1,9 +1,9 @@
 class CreateHealths < ActiveRecord::Migration[6.1]
   def change
-    create_table :healths, {comment: "健康"} do |t|
-      t.string  :name,          {limit: 10, null: false, comment: "原価種別名称"}
-      t.string :code,           {limit: 1, null: false, comment: "コード"}
-      t.integer :display_order, {limit: 4, null: false, default: 0, comment: "表示順"}
+    create_table :healths, comment: "健康" do |t|
+      t.string  :name,          limit: 10, null: false, comment: "原価種別名称"
+      t.string :code,           limit: 1, null: false, comment: "コード"
+      t.integer :display_order, limit: 4, null: false, default: 0, comment: "表示順"
       t.timestamps
       t.datetime :deleted_at
     end
@@ -13,7 +13,7 @@ class CreateHealths < ActiveRecord::Migration[6.1]
     Health.create(name: '下痢', code: '4', display_order: 4)
     Health.create(name: 'その他', code: '5', display_order: 5)
 
-    add_column :work_results, :health_id, :integer, {null: false, default: 0, comment: "健康"}
-    add_column :work_results, :remarks, :string, {null: false, limit: 20, default: "", comment: "備考"}
+    add_column :work_results, :health_id, :integer, null: false, default: 0, comment: "健康"
+    add_column :work_results, :remarks, :string, null: false, limit: 20, default: "", comment: "備考"
   end
 end
--- db/migrate/20211211032607_create_machine_remarks.rb
+++ db/migrate/20211211032607_create_machine_remarks.rb
@@ -1,12 +1,12 @@
 class CreateMachineRemarks < ActiveRecord::Migration[6.1]
   def change
-    create_table :machine_remarks, {comment: "作業機械備考"} do |t|
-      t.integer :work_id,     {null: false, comment: "作業"}
-      t.integer :machine_id,  {null: false, comment: "機械"}
-      t.string  :remarks,     {limit: 30, null: false, default: "", comment: "備考"}
+    create_table :machine_remarks, comment: "作業機械備考" do |t|
+      t.integer :work_id,     null: false, comment: "作業"
+      t.integer :machine_id,  null: false, comment: "機械"
+      t.string  :remarks,     limit: 30, null: false, default: "", comment: "備考"
 
       t.timestamps
     end
-    add_index :machine_remarks, [:work_id, :machine_id], {unique: true, name: "machine_remarks_2nd"}
+    add_index :machine_remarks, [:work_id, :machine_id], unique: true, name: "machine_remarks_2nd"
   end
 end
