19b20fc9cbb63a09bcf2fa7c128b37678506bcda
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2022-01-15 16:51:12 +0900

    #206 フレコン

--- app/models/adjustment.rb
+++ app/models/adjustment.rb
@@ -2,17 +2,18 @@
 #
 # Table name: adjustments
 #
-#  id                       :bigint           not null, primary key
-#  carried_on(搬入日)       :date
-#  fixed_amount(確定額)     :decimal(7, )
-#  half_weight(半端米(kg))  :decimal(3, 1)
-#  rice_bag(調整米(袋))     :decimal(3, )
-#  shipped_on(出荷日)       :date
-#  waste_weight(くず米(kg)) :decimal(5, 1)
-#  created_at               :datetime         not null
-#  updated_at               :datetime         not null
-#  drying_id(乾燥)          :integer          default(0), not null
-#  home_id(担当世帯)        :integer
+#  id                             :bigint           not null, primary key
+#  carried_on(搬入日)             :date
+#  container_flag(フレコンフラグ) :boolean          default(FALSE), not null
+#  fixed_amount(確定額)           :decimal(7, )
+#  half_weight(半端米(kg))        :decimal(3, 1)
+#  rice_bag(調整米(袋))           :decimal(3, )
+#  shipped_on(出荷日)             :date
+#  waste_weight(くず米(kg))       :decimal(5, 1)
+#  created_at                     :datetime         not null
+#  updated_at                     :datetime         not null
+#  drying_id(乾燥)                :integer          default(0), not null
+#  home_id(担当世帯)              :integer
 #
 # Indexes
 #
@@ -24,6 +25,10 @@ class Adjustment < ApplicationRecord
 
   def rice_weight(system)
     (rice_bag || 0) * Drying::KG_PER_BAG_RICE + (system.half_sum_flag ? (half_weight || 0) : 0)
+  end
+
+  def container_weight
+    (rice_bag || 0) * Drying::KG_PER_BAG_RICE + (half_weight || 0)
   end
 
   def waste_bag
