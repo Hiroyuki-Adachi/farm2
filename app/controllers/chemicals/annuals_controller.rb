29bf92c8f659aca4818cd47f918f5abaf3581151
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2022-02-04 20:28:46 +0900

    #46 薬剤TEST

--- app/controllers/chemicals/annuals_controller.rb
+++ app/controllers/chemicals/annuals_controller.rb
@@ -1,5 +1,6 @@
 class Chemicals::AnnualsController < ApplicationController
   include PermitManager
+  include PermitLastTerm
 
   def create
     ActiveRecord::Base.transaction do
--- test/controllers/chemicals/annuals_controller_test.rb
+++ test/controllers/chemicals/annuals_controller_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
-class Chemicals::AnnualsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class Chemicals::AnnualsControllerTest < ActionController::TestCase
+  setup do
+    setup_ip
+  end
+
+  test "薬剤年次更新(最新年度以外)" do
+    post :create
+    assert_response :error
+  end
+
+  test "薬剤年次更新(管理者以外)" do
+    session[:user_id] = users(:user2017c).id
+    post :create
+    assert_response :error
+  end
+
+  test "薬剤年次更新" do
+    session[:user_id] = users(:user2017).id
+    assert_difference('ChemicalTerm.count', ChemicalTerm.where(term: 2016).count) do
+      post :create
+    end
+    assert_redirected_to chemicals_path
+  end
 end
