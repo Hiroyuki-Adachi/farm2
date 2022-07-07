30855b21866b5ee56c83dbc1f022d1331e3528d9
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2022-07-07 21:21:55 +0900

    #174 削除機能

--- frontend/javascripts/sorimachi.js
+++ frontend/javascripts/sorimachi.js
@@ -3,6 +3,16 @@ import { Modal } from "bootstrap";
 window.addEventListener("load", () => {
   const popupForm = new Modal(document.getElementById("kamoku_modal"));
 
+  const getCsrfToken = () => {
+    const metas = document.getElementsByTagName('meta');
+    for (let meta of metas) {
+      if (meta.getAttribute('name') === 'csrf-token') {
+        return meta.getAttribute('content');
+      }
+    }
+    return '';
+  }
+
   document.querySelectorAll("button.has-details").forEach((element) => {
       element.addEventListener("click", (event) => {
         const dispFlag = (element.dataset.details != "true");
@@ -28,5 +38,22 @@ window.addEventListener("load", () => {
 
   document.getElementById("kamoku_close").addEventListener("click", () => {
     popupForm.hide();
+  });
+
+  document.getElementById("kamoku_delete").addEventListener("click", (event) => {
+    const journal_id = document.getElementById("sorimachi_id").value;
+    fetch(event.target.dataset.url.replace("@", journal_id), {
+      method: "DELETE",
+      headers: {
+        'X-CSRF-Token': getCsrfToken()
+      }
+    })
+    .then((response) => {
+      return response.text();
+    })
+    .then((data) => {
+      document.getElementById(`tr_${journal_id}`).innerHTML = data;
+      popupForm.hide();
+    });
   });
 });
