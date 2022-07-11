12ae5972e1cbc8e84134a80dc0cdff958e36c743
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2022-07-11 22:13:48 +0900

    #174 test

--- test/controllers/sorimachi/imports_controller_test.rb
+++ test/controllers/sorimachi/imports_controller_test.rb
@@ -11,9 +11,26 @@ class Sorimachi::ImportsControllerTest < ActionController::TestCase
   end
 
   test "農業簿記インポート(実行)" do
+    SorimachiJournal.where(term: 2015).destroy_all
     assert_difference('SorimachiJournal.count', 2) do
       post :create, params: {import_file: fixture_file_upload("#{Rails.root}/test/fixtures/files/sorimachi.csv", 'text/csv')}
     end
     assert_redirected_to sorimachi_imports_path
+  end
+
+  test "農業簿記フラグ設定" do
+    journal = sorimachi_journals(:journal2)
+    assert_no_difference('SorimachiJournal.count') do
+      put :update, params: {id: journal.id}
+    end
+    assert SorimachiJournal.find(journal.id).cost0_flag
+  end
+
+  test "農業簿記フラグ削除" do
+    journal = sorimachi_journals(:journal1)
+    assert_no_difference('SorimachiJournal.count') do
+      delete :destroy, params: {id: journal.id}
+    end
+    assert !SorimachiJournal.find(journal.id).cost0_flag
   end
 end
