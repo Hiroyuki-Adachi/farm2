cb06925f6aeef630412019797daf6ec0865a9901
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2022-07-14 21:34:56 +0900

    #174 簡易集計

--- app/models/sorimachi_journal.rb
+++ app/models/sorimachi_journal.rb
@@ -46,6 +46,7 @@ class SorimachiJournal < ApplicationRecord
   after_update :clear_work_types
   scope :usual, ->(term) {where(term: term, detail: 1).order(:line)}
   scope :cost, ->(term) {where(term: term, cost0_flag: true).order(:line, :detail)}
+  scope :total, ->(term) {where(term: term, cost0_flag: true).order(:code01).group(:code01).sum(:amount1)}
 
   has_many :sorimachi_work_types, dependent: :destroy
   has_many :work_types, through: :sorimachi_work_types
---
+++ test/controllers/sorimachi/totals_controller_test.rb
@@ -0,0 +1,4 @@
+require "test_helper"
+
+class Sorimachi::TotalsControllerTest < ActionController::TestCase
+end
