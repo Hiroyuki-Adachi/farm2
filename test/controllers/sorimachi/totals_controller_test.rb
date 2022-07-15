600bd9acd6218e13717464e9240627a709a82e20
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2022-07-16 00:05:08 +0900

    #174 簡易集計TEST

--- test/controllers/sorimachi/totals_controller_test.rb
+++ test/controllers/sorimachi/totals_controller_test.rb
@@ -1,4 +1,12 @@
 require "test_helper"
 
 class Sorimachi::TotalsControllerTest < ActionController::TestCase
+  setup do
+    setup_ip
+  end
+
+  test "農業簿記簡易集計(表示)" do
+    get :index
+    assert_response :success
+  end
 end
--- test/controllers/sorimachi/work_types_controller_test.rb
+++ test/controllers/sorimachi/work_types_controller_test.rb
@@ -3,5 +3,11 @@ require "test_helper"
 class Sorimachi::WorkTypesControllerTest < ActionController::TestCase
   setup do
     setup_ip
+    @journal = sorimachi_journals(:journal1)
+  end
+
+  test "農業簿記内訳(表示)" do
+    get :edit, params: {sorimachi_journal_id: @journal}
+    assert_response :success
   end
 end
