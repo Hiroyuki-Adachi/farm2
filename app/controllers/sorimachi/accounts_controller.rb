728e0496c3724798f181b43056723cd0d5b473ce
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2022-07-23 21:42:42 +0900

    #274 勘定科目保守

--- app/controllers/sorimachi/accounts_controller.rb
+++ app/controllers/sorimachi/accounts_controller.rb
@@ -1,5 +1,6 @@
 class Sorimachi::AccountsController < ApplicationController
   include PermitManager
+  before_action :set_sorimachi_account, only: [:edit, :update, :destroy]
 
   def index
     @journals = SorimachiJournal.accounts(current_term)
@@ -20,7 +21,36 @@ class Sorimachi::AccountsController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    @account.attributes = sorimachi_account_params
+    if @account.save
+      redirect_to sorimachi_accounts_path
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    @account.destroy unless @account.new_record?
+    redirect_to sorimachi_accounts_path
+  end
+
+  private
+
+  def set_sorimachi_account
     @account = SorimachiAccount.find_by(term: current_term, code: params[:code])
     @account = SorimachiAccount.new(term: current_term, code: params[:code]) unless @account
+  end
+
+  def sorimachi_account_params
+    params.require(:sorimachi_account)
+      .permit(
+        :term,
+        :code,
+        :name,
+        :cost_flag
+      )
   end
 end
