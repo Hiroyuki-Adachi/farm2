ca91610a4731fc6b01c32e84ff9b9b99afe95fc9
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2022-09-15 21:30:12 +0900

    #282 manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,1 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link_directory ../../javascript/javascript .js
-//= link_tree ../../../vendor/javascript .js
-//= link_tree ../../javascript/controllers .js
-//= link_tree ../stylesheets .scss
