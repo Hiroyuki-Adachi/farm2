754bc2f587ac736024f9435c2567ee31c0d1bbfc
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2022-12-01 20:38:12 +0900

    #240 研修記録 (junction)

--- app/models/training.rb
+++ app/models/training.rb
@@ -1,2 +1,4 @@
 class Training < ApplicationRecord
+  has_many :training_training_types, dependent: :destroy
+  has_many :training_types, through: :training_training_types
 end
---
+++ app/models/training_training_type.rb
@@ -0,0 +1,4 @@
+class TrainingTrainingType < ApplicationRecord
+  belongs_to :traning_type
+  belongs_to :traning
+end
