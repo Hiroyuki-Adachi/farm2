9a7991d970db9dcb83692046f398022e686c699c
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2022-12-03 17:08:30 +0900

    #307

--- app/models/cleaning.rb
+++ app/models/cleaning.rb
@@ -2,12 +2,15 @@
 #
 # Table name: cleanings
 #
-#  id               :bigint           not null, primary key
-#  method(清掃方法) :string(20)       default(""), not null
-#  target(駆除対象) :string(20)       default(""), not null
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
-#  work_id(作業ID)  :integer          not null
+#  id                          :bigint           not null, primary key
+#  animal_flag(動物駆除フラグ) :boolean          default(FALSE), not null
+#  cleaning_flag(清掃フラグ)   :boolean          default(FALSE), not null
+#  method(清掃方法)            :string(20)       default(""), not null
+#  pest_flag(害虫駆除フラグ)   :boolean          default(FALSE), not null
+#  target(駆除対象)            :string(20)       default(""), not null
+#  created_at                  :datetime         not null
+#  updated_at                  :datetime         not null
+#  work_id(作業ID)             :integer          default(0), not null
 #
 class Cleaning < ApplicationRecord
   belongs_to :work
