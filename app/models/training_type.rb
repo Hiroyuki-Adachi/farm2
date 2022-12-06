2fdfa059fa6119aba130aa140b355ea432df4e7a
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2022-12-06 21:42:29 +0900

    #240 研修記録 organization保守

--- app/models/training_type.rb
+++ app/models/training_type.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: training_types
+#
+#  id                       :bigint           not null, primary key
+#  display_order(表示順)    :integer          not null
+#  name(名称)               :string(10)       not null
+#  other_flag(その他フラグ) :boolean          default(FALSE), not null
+#  created_at               :datetime         not null
+#  updated_at               :datetime         not null
+#
 class TrainingType < ApplicationRecord
 end
