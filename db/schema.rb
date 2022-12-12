e9427e565642984ad3da314810293dc24bc08b75
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2022-12-12 21:20:56 +0900

    #240 change migration

--- db/schema.rb
+++ db/schema.rb
@@ -818,7 +818,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_122756) do
     t.integer "worker_id", null: false, comment: "講師(作業者ID)"
     t.string "content", limit: 20, default: "", null: false, comment: "内容"
     t.string "document", limit: 40, default: "", null: false, comment: "資料"
-    t.string "place", limit: 20, default: "", null: false, comment: "場所"
+    t.string "training_place", limit: 20, default: "", null: false, comment: "研修場所"
+    t.string "studying_place", limit: 20, default: "", null: false, comment: "学習場所"
+    t.text "remarks", default: "", null: false, comment: "備考"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
