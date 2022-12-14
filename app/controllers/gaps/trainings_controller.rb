24a422b97da6714fc45957b2529ec1189ee098ea
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2022-12-14 22:09:42 +0900

    #240 研修登録画面(途中)

--- app/controllers/gaps/trainings_controller.rb
+++ app/controllers/gaps/trainings_controller.rb
@@ -1,4 +1,6 @@
 class Gaps::TrainingsController < GapsController
+  before_action :set_traning, only: [:show, :edit, :update, :destroy]
+
   def index
     @works = WorkDecorator.decorate_collection(
       Work.by_term(current_term).where(work_kind_id: current_organization.training_id)
@@ -16,5 +18,12 @@ class Gaps::TrainingsController < GapsController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_traning
+    @work = Work.find(params[:id])
+    @training = @work&.training || Training.new
   end
 end
