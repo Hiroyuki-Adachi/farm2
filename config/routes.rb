03c33c51527dbb725bc605d0317eb145a6f2c877
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2022-12-21 21:28:00 +0900

    #240 ヒヤリハット 入力欄連動

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,12 @@ Rails.application.routes.draw do
     resources :maintenances, only: [:index]
     resources :cleanings, only: [:index, :edit, :update]
     resources :trainings, only: [:index, :show, :edit, :update, :destroy]
-    resources :accidents
+    resources :accidents do
+      member do
+        get 'works/:worked_at', to: 'accidents#works', as: 'works'
+        get 'audiences/:work_id', to: 'accidents#audiences', as: 'audiences'
+      end
+    end
   end
   resources :work_seedlings, only: [:index]
   resources :owned_rices, only: [:index, :edit, :update]
