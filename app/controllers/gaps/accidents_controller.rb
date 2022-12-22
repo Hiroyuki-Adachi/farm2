7cd724c44cc5f1088986020dcffe0fe0ec1ae9bf
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2022-12-22 21:20:14 +0900

    #240 new create

--- app/controllers/gaps/accidents_controller.rb
+++ app/controllers/gaps/accidents_controller.rb
@@ -1,5 +1,6 @@
 class Gaps::AccidentsController < GapsController
   before_action :set_accident, only: [:show, :edit, :update, :destroy]
+  helper GmapHelper
 
   def index
     @accidents = Accident.usual(current_term)
@@ -9,19 +10,32 @@ class Gaps::AccidentsController < GapsController
   end
 
   def new
-    @accident = Accident.new
+    @accident = Accident.new(accident_type_id: AccidentType::RULE.id)
   end
 
   def create
+    @accident = Accident.new(accident_params)
+    if @accident.save
+      redirect_to gaps_accidents_path
+    else
+      render action: :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @accident.update(accident_params)
+      redirect_to gaps_accidents_path
+    else
+      render action: :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @accident.destroy
+    redirect_to gaps_accidents_path, status: :see_other
   end
 
   def works
@@ -40,5 +54,23 @@ class Gaps::AccidentsController < GapsController
 
   def set_accident
     @accident = Accident.find(params[:id])
+  end
+
+  def accident_params
+    params.require(:accident)
+          .permit(
+            :investigator_id,
+            :investigated_on,
+            :informant_name,
+            :accident_type_id,
+            :work_id,
+            :audience_id,
+            :location,
+            :location_name,
+            :content,
+            :problem,
+            :solving,
+            :result
+          )
   end
 end
---
+++ frontend/javascripts/gap-accidents.js
@@ -0,0 +1,32 @@
+window.addEventListener('turbo:load', () => {
+    initMap();
+
+    document.getElementById("worked_at").addEventListener("blur", (event) => {
+        Turbo.visit(document.getElementById("works_gaps_accident_path").value.replace(":worked_at", event.target.value));
+    });
+    document.getElementById("accident_work_id").addEventListener("change", () => {
+        Turbo.visit(document.getElementById("audiences_gaps_accident_path").value.replace(":work_id", event.target.value));
+    });
+});
+
+function initMap(){
+    const location = document.getElementById("accident_location");
+    const home = JSON.parse(location.value.replace("(", "[").replace(")", "]"));
+    const pos = new google.maps.LatLng(home[0], home[1])
+  
+    const map = new google.maps.Map(document.getElementById('map'), {
+      center: pos,
+      zoom: 16,
+      mapTypeId: google.maps.MapTypeId.SATELLITE
+    });
+  
+    const marker = new google.maps.Marker({
+      position: pos,
+      draggable: true,
+      map: map
+    });
+  
+    marker.addListener("dragend", function(arg) {
+      location.value = arg.latLng.toString();
+    });
+}
