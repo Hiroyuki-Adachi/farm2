25661ec0866a541a0a0babec83cb198a059eaa85
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2023-01-02 00:04:32 +0900

    #282 test OK

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,5 @@
 //= link_tree ../images
 //= link_tree ../../javascript/controllers
-//= link_tree ../../javascript/lib
 //= link pc-common.css
 //= link calendar.css
 //= link i18n-js/translations.js
