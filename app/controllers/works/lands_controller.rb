bc9941a6ce638009d00ddefc45a62550b38c1f4d
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2023-01-07 01:29:43 +0900

    #282 autocomplete

--- app/controllers/works/lands_controller.rb
+++ app/controllers/works/lands_controller.rb
@@ -1,5 +1,5 @@
 class Works::LandsController < WorksController
-  before_action :set_work, only: [:new, :create]
+  before_action :set_work, only: [:new, :create, :autocomplete]
   before_action :set_lands, only: [:new]
   before_action :check_fixed, only: [:new, :create]
   before_action :permit_checkable_or_self, only: [:new, :create]
@@ -10,5 +10,9 @@ class Works::LandsController < WorksController
   def create
     @work.regist_lands(params[:work_lands] || [])
     redirect_to(work_path(@work))
+  end
+
+  def autocomplete
+    render json: Land.to_autocomplete(Land.expiry(@work.model.worked_at).includes(:owner))
   end
 end
