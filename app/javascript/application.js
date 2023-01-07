a1a59340ac978ca1132905ee3a51dff1acc0d791
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2023-01-08 00:36:55 +0900

    #313 personal_information側

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "bootstrap";
-
-import moment from 'moment';
-window.moment = moment;
-
-import I18n from 'i18n-js/translations';
-window.I18n = I18n;
