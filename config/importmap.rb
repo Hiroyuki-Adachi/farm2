bc98d9d32593895a284c9faaee52a79f6f841bb8
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2023-02-03 20:34:36 +0900

    2023-02-03

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,11 +1,9 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "@hotwired/turbo", to: "https://ga.jspm.io/npm:@hotwired/turbo@7.2.4/dist/turbo.es2017-esm.js"
 pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.2.1/js/fontawesome.js"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/lib/index.js"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js"
-pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.2.0/dist/chart.js"
 pin "chart.js/auto", to: "https://ga.jspm.io/npm:chart.js@4.1.2/auto/auto.js"
 pin "decimal.js", to: "https://ga.jspm.io/npm:decimal.js@10.4.2/decimal.mjs"
 pin "i18n-js", to: "https://ga.jspm.io/npm:i18n-js@4.2.2/dist/import/index.js"
@@ -16,9 +14,6 @@ pin "buffer", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.26/nodelibs/brow
 pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.26/nodelibs/browser/process-production.js"
 pin "randombytes", to: "https://ga.jspm.io/npm:randombytes@2.1.0/browser.js"
 pin "safe-buffer", to: "https://ga.jspm.io/npm:safe-buffer@5.2.1/index.js"
-pin "@hotwired/turbo-rails", to: "https://ga.jspm.io/npm:@hotwired/turbo-rails@7.2.4/app/javascript/turbo/index.js"
-pin "@rails/actioncable/src", to: "https://ga.jspm.io/npm:@rails/actioncable@7.0.4/src/index.js"
-pin "@kurkle/color", to: "https://ga.jspm.io/npm:@kurkle/color@0.3.2/dist/color.esm.js"
 pin "lodash", to: "https://ga.jspm.io/npm:lodash@4.17.21/lodash.js"
 pin "lodash/get", to: "https://ga.jspm.io/npm:lodash@4.17.21/get.js"
 pin "lodash/has", to: "https://ga.jspm.io/npm:lodash@4.17.21/has.js"
@@ -34,4 +29,9 @@ pin "lodash/flattenDeep", to: "https://ga.jspm.io/npm:lodash@4.17.21/flattenDeep
 pin "lodash/range", to: "https://ga.jspm.io/npm:lodash@4.17.21/range.js"
 pin "sortablejs", to: "https://ga.jspm.io/npm:sortablejs@1.15.0/modular/sortable.esm.js"
 pin "@tarekraafat/autocomplete.js", to: "https://ga.jspm.io/npm:@tarekraafat/autocomplete.js@10.2.7/dist/autoComplete.min.js"
+pin "@hotwired/turbo", to: "https://ga.jspm.io/npm:@hotwired/turbo@7.2.5/dist/turbo.es2017-esm.js"
+pin "@hotwired/turbo-rails", to: "https://ga.jspm.io/npm:@hotwired/turbo-rails@7.2.5/app/javascript/turbo/index.js"
+pin "@rails/actioncable/src", to: "https://ga.jspm.io/npm:@rails/actioncable@7.0.4/src/index.js"
+pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.2.0/dist/chart.js"
+pin "@kurkle/color", to: "https://ga.jspm.io/npm:@kurkle/color@0.3.2/dist/color.esm.js"
 pin_all_from "app/javascript/i18n-js", under: "i18n-js"
