a4d4acd3b886ed87a07326f78d060c684f90fd59
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2023-05-04 16:23:39 +0900

    #316 test

--- test/controllers/chemical_costs_controller_test.rb
+++ test/controllers/chemical_costs_controller_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class ChemicalCostsControllerTest < ActionController::TestCase
   setup do
     setup_ip
+    @chemical_term_id = chemical_terms(:chemical_term_3_2015).id
+    @chemical_work_type_genka3 = chemical_work_types(:chemical_work_type_genka3)
   end
 
   test "薬剤原価" do
@@ -16,27 +18,81 @@ class ChemicalCostsControllerTest < ActionController::TestCase
     assert_response :error
   end
 
-  test "薬剤原価新規作成(実行)" do
-    chemical_term_id = chemical_terms(:chemical_term_3_2015).id
-    work_type_id = work_types(:work_type_koshi).id
-    chemical_price = 5500
-    quantity = 12.3
-    assert_no_difference('ChemicalTerm.count') do
-      assert_difference('ChemicalWorkType.where.not(quantity: 0).count') do
-        post :create, params: {
-          chemical_terms: [{id: chemical_term_id, price: chemical_price}],
-          chemical_work_types: [{
-            work_type_id: work_type_id,
-            chemical_term_id: chemical_term_id,
-            quantity: quantity
-          }]
-        }
-      end
-    end
-    assert_redirected_to chemical_costs_path
+  test "薬剤原価新規作成(表示)" do
+    get :new, params: {chemical_term_id: @chemical_term_id, work_type_id: 1}, as: :turbo_stream
+    assert_response :success
+    assert_equal "text/vnd.turbo-stream.html; charset=utf-8", response.content_type
+  end
 
-    assert_equal chemical_price, ChemicalTerm.find(chemical_term_id).price
-    chemical_work_type = ChemicalWorkType.find_by(work_type_id: work_type_id, chemical_term_id: chemical_term_id)
-    assert_equal quantity, chemical_work_type.quantity
+  test "薬剤原価新規作成(実行)" do
+    assert_difference('ChemicalWorkType.count') do
+      post :create, params: {
+        chemical_work_type: {chemical_term_id: @chemical_term_id, work_type_id: 1, quantity: 1},
+        regist: true
+      }, as: :turbo_stream
+    end
+
+    assert_no_difference('ChemicalWorkType.count') do
+      post :create, params: {
+        chemical_work_type: {chemical_term_id: @chemical_term_id, work_type_id: 2, quantity: 1},
+        back: true
+      }, as: :turbo_stream
+    end
+
+    assert_no_difference('ChemicalWorkType.count') do
+      post :create, params: {
+        chemical_work_type: {chemical_term_id: @chemical_term_id, work_type_id: 4, quantity: 0},
+        regist: true
+      }, as: :turbo_stream
+    end
+  end
+
+  test "薬剤原価更新(表示)" do
+    get :edit, params: {id: @chemical_work_type_genka3.id}, as: :turbo_stream
+    assert_response :success
+    assert_equal "text/vnd.turbo-stream.html; charset=utf-8", response.content_type
+  end
+
+  test "薬剤原価更新(実行)" do
+    assert_no_difference('ChemicalWorkType.count') do
+      patch :update, params: {
+        id: @chemical_work_type_genka3.id, 
+        chemical_work_type: {
+          chemical_term_id: @chemical_work_type_genka3.chemical_term_id,
+          work_type_id: @chemical_work_type_genka3.work_type_id,
+          quantity: 2
+        },
+        regist: true
+      }, as: :turbo_stream
+    end
+    assert_equal 2, ChemicalWorkType.find(@chemical_work_type_genka3.id).quantity
+    assert_response :success
+
+    assert_no_difference('ChemicalWorkType.count') do
+      patch :update, params: {
+        id: @chemical_work_type_genka3.id, 
+        chemical_work_type: {
+          chemical_term_id: @chemical_work_type_genka3.chemical_term_id,
+          work_type_id: @chemical_work_type_genka3.work_type_id,
+          quantity: 3
+        },
+        back: true
+      }, as: :turbo_stream
+    end
+    assert_not_equal 3, ChemicalWorkType.find(@chemical_work_type_genka3.id).quantity
+    assert_response :success
+
+    assert_difference('ChemicalWorkType.count', -1) do
+      patch :update, params: {
+        id: @chemical_work_type_genka3.id, 
+        chemical_work_type: {
+          chemical_term_id: @chemical_work_type_genka3.chemical_term_id,
+          work_type_id: @chemical_work_type_genka3.work_type_id,
+          quantity: 0
+        },
+        regist: true
+      }, as: :turbo_stream
+    end
+    assert_response :success
   end
 end
