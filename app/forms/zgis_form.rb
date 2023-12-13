283b201108bda4d4e5dcc07287ab97e2d22a4a30
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2023-12-13 22:05:21 +0900

    #357 form/helper

---
+++ app/forms/zgis_form.rb
@@ -0,0 +1,11 @@
+require 'rubyXL'
+require 'rubyXL/convenience_methods/cell'
+
+class ZgisForm
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+
+  def export(term)
+    
+  end
+end
