9cde5db3e270929af6326560e68db1441b7706dd
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2023-12-25 21:56:53 +0900

    #361

--- config/puma.rb
+++ config/puma.rb
@@ -4,12 +4,13 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
+require 'rails'
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
-bind "unix:///tmp/puma_farm2.sock"
-pidfile "/tmp/puma_farm2.pid"
-state_path "/tmp/puma_farm2.state"
+bind "unix:///#{Rails.root}/puma_farm2.sock"
+pidfile "#{Rails.root}/tmp/puma_farm2.pid"
+state_path "#{Rails.root}/tmp/puma_farm2.state"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
