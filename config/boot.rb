3d178967887a182a86b430c74e4578785a4dfa35
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2024-01-10 19:22:37 +0900

    merge develop

--- config/boot.rb
+++ config/boot.rb
@@ -1,7 +1,3 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
-<<<<<<< HEAD
-# require "bootsnap/setup" # Speed up boot time by caching expensive operations.
-=======
->>>>>>> develop
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,17 +56,6 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-<<<<<<< HEAD
-
-  # Raises error for missing translations.
-  # config.i18n.raise_on_missing_translations = true
-
-  # Annotate rendered view with file names.
-  # config.action_view.annotate_rendered_view_with_filenames = true
-
-  # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
-=======
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
 
@@ -84,5 +73,4 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
->>>>>>> develop
 end
