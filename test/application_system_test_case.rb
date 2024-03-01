f49ab0c67f73f1bee7849c4f1d8c7e3b858deba4
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2024-03-01 22:45:08 +0900

    #311 まだエラー

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,18 +1,45 @@
 require "test_helper"
 require "capybara/cuprite"
 
-Capybara.server = :puma, { Silent: true } # テスト出力を減らすために Puma サーバーを Silent モードで使用
-Capybara.javascript_driver = :cuprite
+REMOTE_CHROME_URL = ENV["CHROME_URL"]
+REMOTE_CHROME_HOST, REMOTE_CHROME_PORT =
+  if REMOTE_CHROME_URL
+    URI.parse(REMOTE_CHROME_URL).yield_self do |uri|
+      [uri.host, uri.port]
+    end
+  end
 
-options = {
-  url: 'http://chrome:3333',
-  browser_options: { 'no-sandbox': nil }
-}
+# リモートのChromeが実行中かどうかをチェック
+remote_chrome =
+  begin
+    if REMOTE_CHROME_URL.nil?
+      false
+    else
+      Socket.tcp(REMOTE_CHROME_HOST, REMOTE_CHROME_PORT, connect_timeout: 1).close
+      true
+    end
+  rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH, SocketError
+    false
+  end
 
-Capybara.register_driver :cuprite do |app|
-  Capybara::Cuprite::Driver.new(app, options)
+remote_options = remote_chrome ? { url: REMOTE_CHROME_URL } : {}
+
+Capybara.register_driver(:better_cuprite) do |app|
+  Capybara::Cuprite::Driver.new(
+    app,
+    **{
+      window_size: [1200, 800],
+      browser_options: remote_chrome ? { "no-sandbox" => nil } : {},
+      inspector: true
+    }.merge(remote_options)
+  )
 end
 
+Capybara.server_host = "0.0.0.0"
+Capybara.default_normalize_ws = true
+Capybara.default_driver = Capybara.javascript_driver = :better_cuprite
+Capybara.app_host = "http://#{ENV.fetch('APP_HOST', `hostname`.strip&.downcase || '0.0.0.0')}"
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :cuprite
+  driven_by :better_cuprite
 end
