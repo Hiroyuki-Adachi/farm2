9f6b1ca3423a22abc4547addd6b59948fab39288
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2024-03-02 17:41:46 +0900

    #311 ログインテストOK

--- test/system/logins_test.rb
+++ test/system/logins_test.rb
@@ -1,8 +1,16 @@
 require "application_system_test_case"
 
 class LoginsTest < ApplicationSystemTestCase
-  test "ログインページ表示" do
+  test "ログインからログアウトまで" do
     visit root_path 
-    assert_selector "#login_nam"
+    assert_selector '#login_name'
+
+    fill_in 'login_name', with: '1234567890'
+    fill_in 'password', with: 'password'
+    click_button '認証する'
+    assert_selector 'a', text: '作業日報管理'
+
+    click_link 'ログアウト'
+    assert_selector '#login_name'
   end
 end
