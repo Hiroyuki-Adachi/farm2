3a0d2c45e4884e0b4a393d52588f8a98e0fefebb
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2024-03-04 15:00:10 +0900

    #311 作業登録

---
+++ test/system/works_test.rb
@@ -0,0 +1,34 @@
+require "application_system_test_case"
+
+class WorksTest < ApplicationSystemTestCase
+  test "ログインから日報入力まで" do
+    visit root_path 
+
+    fill_in 'login_name', with: '1234567890'
+    fill_in 'password', with: 'password'
+    click_button '認証する'
+    assert_selector 'a', text: '作業日報管理'
+
+    click_link '日報入力'
+    assert_selector 'h1', text: '作業日報入力'
+
+    fill_in 'work_worked_at', with: Date.new(2015, 5, 5)
+    select '曇り', from: 'work[weather_id]'
+    fill_in 'work_start_at', with: '0900'
+    fill_in 'work_end_at', with: '1500'
+    choose 'work_type_23'
+    select '代掻', from: 'work[work_kind_id]'
+    fill_in 'work_name', with: '作業の内容'
+    fill_in 'work_remarks', with: '作業の記事ですよーーー'
+
+    assert_difference 'Work.count', 1 do
+      click_button '登録'
+    end
+    assert_selector 'h1', text: '作業日報(作業者)登録'
+
+    assert_difference 'WorkResult.count', 1 do
+      click_button 'add_button_1'
+      click_button '登録'
+    end
+  end
+end
