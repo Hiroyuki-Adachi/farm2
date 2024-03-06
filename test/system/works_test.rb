a03726e466f63808d3d2fe18a4b724417335a396
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2024-03-06 10:40:02 +0900

    #311 健康項目

--- test/system/works_test.rb
+++ test/system/works_test.rb
@@ -7,10 +7,10 @@ class WorksTest < ApplicationSystemTestCase
     fill_in 'login_name', with: '1234567890'
     fill_in 'password', with: 'password'
     click_button '認証する'
-    assert_selector 'a', text: '作業日報管理'
+    assert_selector 'a', exact_text: '作業日報管理'
 
     click_link '日報入力'
-    assert_selector 'h1', text: '作業日報入力'
+    assert_selector 'h1', exact_text: '作業日報入力'
 
     fill_in 'work_worked_at', with: Date.new(2015, 5, 5)
     select '曇り', from: 'work[weather_id]'
@@ -24,11 +24,15 @@ class WorksTest < ApplicationSystemTestCase
     assert_difference 'Work.count', 1 do
       click_button '登録'
     end
-    assert_selector 'h1', text: '作業日報(作業者)登録'
+    assert_selector 'h1', exact_text: '作業日報(作業者)登録'
 
     assert_difference 'WorkResult.count', 1 do
       click_button 'add_button_1'
       click_button '登録'
     end
+    assert_selector 'h1', exact_text: '作業日報(健康)登録'
+
+    click_button '登録'
+    assert_selector 'table#detail_workers', text: '後藤 晴美'
   end
 end
