3ad903b04079372230b42e502e9d4c2c290c466c
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2024-03-21 22:30:58 +0900

    #364 test

--- app/controllers/plans/work_types_controller.rb
+++ app/controllers/plans/work_types_controller.rb
@@ -9,11 +9,11 @@ class Plans::WorkTypesController < ApplicationController
 
   def create
     WorkType.transaction do
-      params[:work_types].each do |key, value|
-        work_type = WorkType.find(key)
+      params[:work_types].each do |work_type_id, work_type_params|
+        work_type = WorkType.find(work_type_id)
         work_type.term = next_term
-        work_type.term_flag = value[:term_flag]
-        work_type.bg_color = value[:bg_color]
+        work_type.term_flag = work_type_params[:term_flag]
+        work_type.bg_color = work_type_params[:bg_color]
         work_type.save!
       end
     end
@@ -28,19 +28,5 @@ class Plans::WorkTypesController < ApplicationController
 
   def save_system
     System.init(current_organization.id, current_term + 1).save!
-  end
-
-  def param_work_types
-    params.permit(work_types: [
-      :area, 
-      :month, 
-      :quantity, 
-      :seeds, 
-      :unit,
-      :between_stocks,
-      :soils,
-      :bag_weight1,
-      :bag_weight2
-      ]).require(:work_types)
   end
 end
