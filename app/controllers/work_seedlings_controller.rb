9ba53ebe2774b36b43e03f256e7085256763c986
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2024-04-01 20:29:55 +0900

    #53 EmptyBlock

--- app/controllers/work_seedlings_controller.rb
+++ app/controllers/work_seedlings_controller.rb
@@ -6,8 +6,6 @@ class WorkSeedlingsController < ApplicationController
     @work_seedlings, @work_areas = calc_seedlings(Work.where(work_kind_id: current_organization.rice_planting_id).by_term(@term))
 
     respond_to do |format|
-      format.html do
-      end
       format.csv do
         render :content_type => 'text/csv; charset=cp943'
       end
