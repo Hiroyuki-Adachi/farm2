2e50f57c4c65f4c4e22997070a0faa287939a872
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2024-04-02 19:48:37 +0900

    #53 PercentLiteralDelimiters

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Farm2
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
     config.autoload_paths += Dir["#{config.root}/app/services/concerns"]
     config.eager_load_paths += Dir["#{config.root}/lib/**/"]
