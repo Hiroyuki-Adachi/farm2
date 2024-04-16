faf39e944146d1857532ae7b06137666ecb632a3
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2024-04-16 12:57:36 +0900

    #400 use ipaddr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'ipaddr'
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -6,7 +7,15 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   helper_method :menu_name
 
-  PERMIT_ADDRESSES = ['127.0.0.1', '192.168.', '172.', '10.'].freeze
+  PERMIT_ADDRESSES = [
+    IPAddr.new('127.0.0.1/32'),      # IPv4 local host
+    IPAddr.new('10.0.0.0/8'),        # IPv4 private network(class A)
+    IPAddr.new('172.16.0.0/12'),     # IPv4 private network(class B)
+    IPAddr.new('192.168.0.0/16'),    # IPv4 private network(class C)
+    IPAddr.new('::1/128'),           # IPv6 local host
+    IPAddr.new('fc00::/7'),          # IPv6 unique local addresses
+    IPAddr.new('fe80::/10')          # IPv6 link-local addresses
+  ].freeze
 
   before_action :set_term, if: :user_present?
   before_action :restrict_remote_ip
@@ -62,7 +71,8 @@ class ApplicationController < ActionController::Base
   end
 
   def restrict_remote_ip
-    if PERMIT_ADDRESSES.none? { |pa| request.remote_ip.start_with?(pa)}
+    remote_ip = IPAddr.new(request.remote_ip)
+    if PERMIT_ADDRESSES.none? { |ip| ip.include?(remote_ip) }
       to_error_path
     elsif session[:user_id].nil? && controller_name != "sessions"
       redirect_to root_path
