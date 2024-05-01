01271154d02a139fc4fb90b9081acf6dafc9b566
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2024-05-01 17:34:10 +0900

    #417 service追加

--- app/services/calendar_excel_half_service.rb
+++ app/services/calendar_excel_half_service.rb
@@ -2,7 +2,7 @@ require 'rubyXL'
 require 'rubyXL/convenience_methods/cell'
 require 'rubyXL/convenience_methods/workbook'
 
-class CalendarExcelHaltService
+class CalendarExcelHalfService
   include Workbook
   MAX_MONTHS = 6
 
@@ -15,7 +15,7 @@ class CalendarExcelHaltService
     setup_workbook(workbook)
 
     first_month = works[0].model.worked_at.month
-    fill_title(workbook[0], calendar_work_kinds[0], year, first_month)
+    fill_title(workbook[0], calendar_work_kinds, year, first_month)
     fill_holidays(workbook[1], year, first_month, first_month + MAX_MONTHS - 1)
     fill_works(workbook[0], works, first_month)
 
@@ -24,9 +24,9 @@ class CalendarExcelHaltService
 
   private
 
-  def fill_title(data_sheet, calendar_work_kind, year, first_month)
+  def fill_title(data_sheet, calendar_work_kinds, year, first_month)
     title_cell = data_sheet[3][0]
-    title_cell.change_contents(calendar_work_kind.work_kind.name.to_s)
+    title_cell.change_contents(calendar_work_kinds.map {|calendar_work_kind| calendar_work_kind.work_kind.name }.join('・'))
 
     data_sheet[5][0].change_contents(year)
     data_sheet[5][2].change_contents(first_month)
@@ -38,5 +38,5 @@ class CalendarExcelHaltService
       work_cell = data_sheet[(work.model.worked_at.day * 2) + 4][((work.model.worked_at.month - first_month) * 3) + 2]
       work_cell.change_contents("#{work.exact_work_type_name}(#{work.sum_areas}a)")
     end
-  end      
+  end
 end
