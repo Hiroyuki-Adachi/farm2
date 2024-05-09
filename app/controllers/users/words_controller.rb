fd7ac46b7d19b0bf838663ce55d88784fb5109a8
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2024-05-09 20:33:14 +0900

    #412 flash messages

--- app/controllers/users/words_controller.rb
+++ app/controllers/users/words_controller.rb
@@ -6,9 +6,10 @@ class Users::WordsController < UsersController
   def create
     if current_user.update(user_params)
       logger.debug "create success:#{user_params}"
-      redirect_to new_users_word_path, notice: "Words saved successfully"
+      redirect_to new_users_word_path, notice: "キーワードを登録しました。"
     else
-      render :new
+      flash.now[:alert] = 'キーワードが重複している可能性があります。'
+      render turbo_stream: turbo_stream.update('flash_messages', partial: 'application/flashes')
     end
   end
 
