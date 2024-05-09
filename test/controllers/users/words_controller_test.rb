e34517548ffd42ff62aca1b68f08e34c961ec7a5
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2024-05-09 16:45:00 +0900

    #412 test

--- test/controllers/users/words_controller_test.rb
+++ test/controllers/users/words_controller_test.rb
@@ -8,5 +8,26 @@ class Users::WordsControllerTest < ActionController::TestCase
   test "検索ワード(表示)" do
     get :new
     assert_response :success
-  end  
+  end
+
+  test "検索ワード(保守)(追加パターン)" do
+    assert_difference('UserWord.count', 1) do
+      post :create, params: { user: {user_words_attributes: [
+        {word: "test"}
+      ] }}
+    end
+    assert_redirected_to new_users_word_path
+  end
+
+  test "検索ワード(保守)(空欄パターン)" do
+    word1 = user_words(:words1)
+    word2 = user_words(:words2)
+    assert_difference('UserWord.count', -1) do
+      post :create, params: { user: {user_words_attributes: [
+        {id: word1.id, word: ""},
+        {id: word2.id, word: "TEST"}
+      ] }}
+    end
+    assert_redirected_to new_users_word_path
+  end
 end
