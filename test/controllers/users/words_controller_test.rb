bde63a87ef56e249fca134c47e0a906146aedec0
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2024-05-09 20:41:47 +0900

    #412 test

--- test/controllers/users/words_controller_test.rb
+++ test/controllers/users/words_controller_test.rb
@@ -30,4 +30,15 @@ class Users::WordsControllerTest < ActionController::TestCase
     end
     assert_redirected_to new_users_word_path
   end
+
+  test "検索ワード(保守)(重複パターン)" do
+    word1 = user_words(:words1)
+    word2 = user_words(:words2)
+    assert_raises(ActiveRecord::RecordNotUnique) do
+      post :create, params: { user: {user_words_attributes: [
+        {id: word1.id, word: "TEST"},
+        {id: word2.id, word: "TEST"}
+      ] }}
+    end
+  end
 end
