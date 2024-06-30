585ebc74cae12e5188f2486fe3439b12526b6c81
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2024-06-30 16:08:04 +0900

    #469 不要なinitializer 削除

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,6 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join("app/javascript")
-Rails.application.config.assets.paths << Rails.root.join("vendor/javascript") if Rails.env.development?
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
