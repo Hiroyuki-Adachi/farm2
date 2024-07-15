7c93882a63d24a91b360f714b587d3da4d15b0c1
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2024-07-15 20:05:07 +0900

    #447 顔認証TEST

--- test/controllers/sessions/faces_controller_test.rb
+++ test/controllers/sessions/faces_controller_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
-class Sessions::FacesControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get sessions_faces_create_url
+class Sessions::FacesControllerTest < ActionController::TestCase
+  setup do
+    setup_ip
+    @face = face_descriptors(:face1)
+  end
+
+  test "Tablet(顔認証)" do
+    post :create, params: {face_descriptor: @face.descriptor.each_with_index.to_h { |value, index| [index.to_s, value] } }
     assert_response :success
   end
 end
