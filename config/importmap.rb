73e9a2b2229e5648048105201b009a0e41038c28
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2024-10-28 22:15:41 +0900

    2024-10-28

--- config/importmap.rb
+++ config/importmap.rb
@@ -21,7 +21,7 @@ pin "decimal.js" # @10.4.3
 pin "base64-js" # @1.5.1
 pin "ieee754" # @1.2.1
 pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.4
-pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @7.1.3
 pin "chart.js/auto", to: "https://ga.jspm.io/npm:chart.js@4.4.3/auto/auto.js"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
+pin "@rails/actioncable", to: "@rails--actioncable.js" # @7.2.102
