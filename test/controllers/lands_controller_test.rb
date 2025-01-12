215e854648b0b2324a10b0b5ac2c8fd35e4fa1bd
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2025-01-12 12:19:16 +0900

    #52 #504 土地マスタ保守

--- test/controllers/lands_controller_test.rb
+++ test/controllers/lands_controller_test.rb
@@ -1,55 +1,74 @@
 require 'test_helper'
 
-class LandsControllerTest < ActionController::TestCase
+class LandsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    setup_ip
+    login_as(users(:users1))
     @land = lands(:lands1)
     @update = {
-      place: "9999", owner_id: Home.first, manager_id: Home.first, area: 55.5,
+      place: "9999", owner_id: Home.first.id, manager_id: Home.first.id, area: 55.5,
       display_order: 99, target_flag: true, reg_area: 66.6
     }
   end
 
   test "土地マスタ一覧" do
-    get :index
+    get lands_path
     assert_response :success
   end
 
   test "土地マスタ一覧(検証者以外)" do
-    session[:user_id] = users(:user_user).id
-    get :index
+    login_as(users(:user_user))
+    get lands_path
     assert_response :error
   end
 
   test "土地マスタ新規作成(表示)" do
-    get :new
+    get new_land_path
     assert_response :success
   end
 
   test "土地マスタ新規作成(実行)" do
     assert_difference('Land.count') do
-      post :create, params: {land: @update}
+      post lands_path, params: {land: @update}
     end
-
     assert_redirected_to lands_path
+
+    land = Land.last
+    assert_equal @update[:place], land.place
+    assert_equal @update[:owner_id], land.owner_id
+    assert_equal @update[:manager_id], land.manager_id
+    assert_equal @update[:area], land.area
+    assert_equal @update[:display_order], land.display_order
+    assert_equal @update[:target_flag], land.target_flag
+    assert_equal @update[:reg_area], land.reg_area
   end
 
   test "土地マスタ変更(表示)" do
-    get :edit, params: {id: @land}
+    get edit_land_path(@land)
     assert_response :success
   end
 
   test "土地マスタ変更(実行)" do
     assert_no_difference('Land.count') do
-      patch :update, params: {id: @land, land: @update}
+      patch land_path(@land), params: {id: @land, land: @update}
     end
     assert_redirected_to lands_path
+
+    @land.reload
+    assert_equal @update[:place], @land.place
+    assert_equal @update[:owner_id], @land.owner_id
+    assert_equal @update[:manager_id], @land.manager_id
+    assert_equal @update[:area], @land.area
+    assert_equal @update[:display_order], @land.display_order
+    assert_equal @update[:target_flag], @land.target_flag
+    assert_equal @update[:reg_area], @land.reg_area
   end
 
   test "土地マスタ削除" do
     assert_difference('Land.count', -1) do
-      delete :destroy, params: {id: @land}
+      delete land_path(@land)
     end
     assert_redirected_to lands_path
+
+    assert_nil Land.find_by(id: @land.id)
   end
 end
