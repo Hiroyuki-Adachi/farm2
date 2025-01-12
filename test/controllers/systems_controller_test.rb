18ad15633c53114c8f64752fbec805925c56df83
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2025-01-12 15:22:21 +0900

    #52 #504 システム保守

--- test/controllers/systems_controller_test.rb
+++ test/controllers/systems_controller_test.rb
@@ -1,26 +1,29 @@
 require 'test_helper'
 
-class SystemsControllerTest < ActionController::TestCase
+class SystemsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    setup_ip
+    login_as(users(:users1))
     @system = systems(:s2015)
   end
 
   test "システム変更画面表示" do
-    get :edit, params: {id: @system}
+    get edit_system_path(@system)
     assert_response :success
   end
 
   test "システム変更(システム管理者以外)" do
-    session[:user_id] = users(:user_manager).id
-    get :edit, params: {id: @system}
+    login_as(users(:user_manager))
+    get edit_system_path(@system)
     assert_response :error
   end
 
   test "システム変更実行" do
     assert_no_difference('System.count') do
-      patch :update, params: {id: @system, system: {dry_price: 1500}}
+      patch system_path(@system), params: {system: {dry_price: 1500}}
     end
     assert_redirected_to menu_index_path
+
+    @system.reload
+    assert_equal 1500, @system.dry_price
   end
 end
