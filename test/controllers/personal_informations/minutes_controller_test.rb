56fccf0ccd500abd1960c0e38ce17cc508edff03
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2025-01-13 21:02:11 +0900

    #504 個人情報

--- test/controllers/personal_informations/minutes_controller_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class PersonalInformations::MinutesControllerTest < ActionController::TestCase
-end
--- test/controllers/personal_informations/works_controller_test.rb
+++ test/controllers/personal_informations/works_controller_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
-class PersonalInformations::WorksControllerTest < ActionController::TestCase
+class PersonalInformations::WorksControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:users1)
   end
 
   test "個人情報(日報)" do
-    session[:user_id] = nil
-    get :show, params: {personal_information_token: @user.token, id: 1478}
+    work = works(:works1)
+    get personal_information_work_path(personal_information_token: @user.token, id: work.id)
     assert_response :success
   end
 end
