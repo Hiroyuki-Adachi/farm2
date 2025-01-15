d0f40d4a82e1667bc77a87395640d4d2a865eacc
Hiroyuki Adachi <hiroyuki-adachi55@gmail.com>
2025-01-15 22:01:30 +0900

    #52 #504 ユーザ

--- test/controllers/users/words_controller_test.rb
+++ test/controllers/users/words_controller_test.rb
@@ -1,48 +1,48 @@
 require "test_helper"
 
-class Users::WordsControllerTest < ActionController::TestCase
+class Users::WordsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    setup_ip
-  end
-
-  test "検索ワード(表示)" do
-    get :new
-    assert_response :success
+    @user = users(:users1)
+    login_as(@user)
   end
 
   test "検索ワード(保守)(追加パターン)" do
+    user_words = [{word: "test"}]
     assert_difference('UserWord.count', 1) do
-      post :create, params: { user: {user_words_attributes: [
-        {word: "test"}
-      ] }}
+      post users_words_path, params: { user: {user_words_attributes: user_words }}
     end
     assert_redirected_to new_users_word_path
 
-    assert_equal "test", UserWord.last.word
+    created_user_word = UserWord.last
+    assert_equal user_words[0][:word], created_user_word.word
+    assert_equal @user.id, created_user_word.user_id
   end
 
   test "検索ワード(保守)(空欄パターン)" do
     word1 = user_words(:words1)
     word2 = user_words(:words2)
+    new_word2 = "TEST"
     assert_difference('UserWord.count', -1) do
-      post :create, params: { user: {user_words_attributes: [
+      post users_words_path, params: { user: {user_words_attributes: [
         {id: word1.id, word: ""},
-        {id: word2.id, word: "TEST"}
+        {id: word2.id, word: new_word2}
       ] }}
     end
     assert_redirected_to new_users_word_path
 
     assert_nil UserWord.find_by(id: word1.id)
-    assert_equal "TEST", UserWord.find_by(id: word2.id).word
+    word2.reload
+    assert_equal new_word2, word2.word
   end
 
   test "検索ワード(保守)(重複パターン)" do
     word1 = user_words(:words1)
     word2 = user_words(:words2)
+    new_word = "TEST"
     assert_raises(ActiveRecord::RecordNotUnique) do
-      post :create, params: { user: {user_words_attributes: [
-        {id: word1.id, word: "TEST"},
-        {id: word2.id, word: "TEST"}
+      post users_words_path, params: { user: {user_words_attributes: [
+        {id: word1.id, word: new_word},
+        {id: word2.id, word: new_word}
       ] }}
     end
   end
