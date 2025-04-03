a41c3450d121a5e03f2b6ab0cbe351f38c3761f5
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2025-04-03 21:48:26 +0900

    完動版

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -106,6 +106,9 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
+  # app root
+  config.relative_url_root = "/farm2"
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
