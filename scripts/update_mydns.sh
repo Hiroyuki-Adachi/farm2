f59ca8d68f51b9a5ee2b13155b67e6be750277e7
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2025-04-12 17:00:16 +0900

    #538 メール廃止

--- scripts/update_mydns.sh
+++ scripts/update_mydns.sh
@@ -6,7 +6,7 @@ source /opt/app/farm2/.env
 set +a
 
 # MyDNS 更新リクエスト
-wget -O - --http-user="${MYDNS_USER}" --http-password="${MYDNS_PASSWORD}" http://ipv4.mydns.jp/login.html
+wget -q -O - --http-user="${MYDNS_USER}" --http-password="${MYDNS_PASSWORD}" http://ipv4.mydns.jp/login.html > /dev/null 2>&1
 
 # 実行ログ
-echo "$(date): MyDNS updated" >> /opt/app/farm2/log/update_mydns.log
+echo "{\"time\": \"$(date -Is)\", \"message\": \"MyDNS updated\"}" >> /opt/app/farm2/log/update_mydns.log
