9dd55ea0111ce7d68be13b503b1be279dce9c370
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2025-04-17 19:51:43 +0900

    #544 OK版

---
+++ app/controllers/health_controller.rb
@@ -0,0 +1,12 @@
+class HealthController < ApplicationController
+  skip_before_action :restrict_remote_ip
+
+  def index
+    Rails.application.config.access_logger.info "GS-Health Check"
+    raise "DB not active" unless ActiveRecord::Base.connection.active?
+    render json: { status: "ok" }, status: :ok
+  rescue StandardError => e
+    Rails.logger.error("Health check failed: #{e.message}")
+    render json: { status: "error", message: e.message }, status: :service_unavailable
+  end
+end
