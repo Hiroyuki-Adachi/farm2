dd4035521cd1eb7e90447adbb6d4e872d39682b0
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2025-04-29 17:53:42 +0900

    #558 地図情報HELPER化

--- app/helpers/gmap_helper.rb
+++ app/helpers/gmap_helper.rb
@@ -11,4 +11,19 @@ module GmapHelper
   def home_location(home)
     return "(#{home.location ? home.location.x : current_organization.location.x},#{home.location ? home.location.y : current_organization.location.y})"
   end
+
+  def land_region_hidden_field(land, color: nil, work_type_id: nil)
+    data = {
+      id: land.id,
+      place: land.place,
+      area: land.area,
+      owner: land.owner.name,
+      manager: land.manager.name
+    }
+
+    data[:color] = color if color
+    data["work-type"] = work_type_id if work_type_id
+  
+    hidden_field_tag :regions, land.region, id: "land_#{land.id}", data: data
+  end
 end
