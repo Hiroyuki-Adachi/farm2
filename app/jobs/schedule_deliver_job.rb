6c48a99855e292a69c0cb1801b35188e306a9412
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2025-05-02 20:06:17 +0900

    #566 ひとまず

---
+++ app/jobs/schedule_deliver_job.rb
@@ -0,0 +1,14 @@
+class ScheduleDeliverJob < ApplicationJob
+  queue_as :default
+
+  def perform
+    User.linable.each do |user|
+      messages = ['明日は以下の予定です。']
+      Schedule.by_worker(user.worker).tommorrow.each do |schedule|
+        messages << "#{schedule.start_at.strftime('%H:%M')}から#{schedule.work_type.name}です。"
+      end
+
+      LineHookService.push_message(user.line_id, messages.join("\n")) if messages.size > 1
+    end
+  end
+end
