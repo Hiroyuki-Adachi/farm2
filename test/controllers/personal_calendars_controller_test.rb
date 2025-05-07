da3b587cf9c26fc10f6858469ecf09839e127de7
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2025-05-07 20:28:48 +0900

    #52 #556 icsテスト強化

--- test/controllers/personal_calendars_controller_test.rb
+++ test/controllers/personal_calendars_controller_test.rb
@@ -3,10 +3,28 @@ require 'test_helper'
 class PersonalCalendarsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:users1)
+    travel_to Time.zone.local(2015, 3, 22)
   end
 
-  test "個人カレンダー" do
+  teardown do
+    travel_back
+  end
+
+  test "カレンダーに必要なデータが含まれている" do
     get personal_calendar_path(token: @user.token)
-    assert response.header["Content-Type"].match(%r{^text/calendar})
+    body = response.body
+  
+    assert_response :success
+    assert_match %r{^text/calendar}, response.header["Content-Type"]
+  
+    assert_includes body, "BEGIN:VEVENT"
+    assert_includes body, schedules(:schedule1).name
+    assert_includes body, works(:works1).remarks
+    assert_includes body, "SUMMARY:"  
+  end
+
+  test "無効なトークンはエラーを返す" do
+    get personal_calendar_path(token: "invalidtoken")
+    assert_response :service_unavailable
   end
 end
