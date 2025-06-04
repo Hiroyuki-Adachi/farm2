0e1984a9e7981602bb41b3b4545a9f088654c0d4
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2025-06-04 19:36:31 +0900

    #598 農村ニュース

--- app/jobs/crawl_nouson_news_job.rb
+++ app/jobs/crawl_nouson_news_job.rb
@@ -1,26 +1,28 @@
 class CrawlNousonNewsJob < CrawlJob
   queue_as :default
 
-  def perform(words)
+  def perform
     agent = Mechanize.new
-    search_all_agri_news(agent, words)
+    doc = Nokogiri::HTML(agent.get(TopicType::NOUSON_NEWS.url).body)
+    doc.css('nav#globalnav_in ul li').each do |nav|
+      search_content(agent, nav.at_css('a')&.[](:href))
+    end
   end
 
   private
 
-  def search_agri_news(agent, word)
-    search_doc = Nokogiri::HTML(agent.get(URI.join(TopicType::NOUSON_NEWS.url, 'media/search').to_s, {schKeyword: word}).body)
-    search_doc.css('div.archivelist_wrap div.blog_contents').each do |content|
+  def search_content(agent, url)
+    doc = Nokogiri::HTML(agent.get(URI.join(TopicType::NOUSON_NEWS.url, url).to_s).body)
+    doc.css('div.archivelist_wrap div.blog_contents').each do |content|
       topic_date = Date.strptime(content.css('span.blog_date01').text, '%Y/%m/%d')
-      next if topic_date < Time.zone.today - START_DAY
-      topic = save_topic(agent, content.at_css('a')&.[](:href), topic_date)
-      save_user_topic(word, topic)
+      break if topic_date < Time.zone.today - START_DAY - 10
+      save_topic(agent, content.at_css('a')&.[](:href), topic_date)
     end
   end
 
   def save_topic(agent, url, topic_date)
     news_doc = Nokogiri::HTML(agent.get(url).body)
-    return Topic.find_or_create_by(url: url) do |topic|
+    Topic.find_or_create_by(url: url) do |topic|
       topic.title = news_doc.at_css('h2.blog_subtitle01')&.text
       news_doc.css('div.txt').css('br').each do |br|
         br.add_next_sibling(Nokogiri::XML::Text.new('　', news_doc))
