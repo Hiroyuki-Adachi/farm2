ee243ef327043eae5925036b252b976d482834d7
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2025-06-04 20:31:10 +0900

    #598 スマートアグリ

--- app/jobs/crawl_nouson_news_job.rb
+++ app/jobs/crawl_nouson_news_job.rb
@@ -15,7 +15,7 @@ class CrawlNousonNewsJob < CrawlJob
     doc = Nokogiri::HTML(agent.get(URI.join(TopicType::NOUSON_NEWS.url, url).to_s).body)
     doc.css('div.archivelist_wrap div.blog_contents').each do |content|
       topic_date = Date.strptime(content.css('span.blog_date01').text, '%Y/%m/%d')
-      break if topic_date < Time.zone.today - START_DAY - 10
+      break if topic_date < Time.zone.today - START_DAY
       save_topic(agent, content.at_css('a')&.[](:href), topic_date)
     end
   end
--- app/jobs/crawl_smart_agri_job.rb
+++ app/jobs/crawl_smart_agri_job.rb
@@ -1,35 +1,28 @@
 class CrawlSmartAgriJob < CrawlJob
   queue_as :default
 
-  def perform(words)
+  def perform
     agent = Mechanize.new
-    search_all_agri_news(agent, words)
+    search_json = JSON.parse(agent.get(URI.join(TopicType::SMART_AGRI.url, 'api/v2/ajax/post').to_s).body, symbolize_names: true)
+    search_json[:data][:posts].each do |topic|
+      topic_date = Time.zone.parse(topic[:published]).to_date
+      break if topic_date < Time.zone.today - START_DAY
+      save_topic(agent, URI.join(TopicType::SMART_AGRI.url, topic[:url]).to_s, topic_date)
+    end
   end
 
   private
 
-  def search_agri_news(agent, word)
-    search_params = {count: 30, offset: 0, keyword: word}
-    search_json = JSON.parse(agent.get(URI.join(TopicType::SMART_AGRI.url, 'api/v2/ajax/post').to_s, search_params).body, symbolize_names: true)
-
-    search_json[:data][:posts].each do |topic|
-      topic_date = Time.zone.parse(topic[:published]).to_date
-      next if topic_date < Time.zone.today - START_DAY
-      topic = save_topic(agent, URI.join(TopicType::SMART_AGRI.url, topic[:url]).to_s, topic_date)
-      save_user_topic(word, topic)
-    end
-  end
-
   def save_topic(agent, url, topic_date)
     news_doc = Nokogiri::HTML(agent.get(url).body)
-    return Topic.find_or_create_by(url: url) do |t|
-      t.title = normalize_text(news_doc.at_css('h1.post-title')&.text)
+    Topic.find_or_create_by(url: url) do |topic|
+      topic.title = normalize_text(news_doc.at_css('h1.post-title')&.text)
       news_doc.css('div.editor-contents').css('br').each do |br|
         br.add_next_sibling(Nokogiri::XML::Text.new('　', news_doc))
       end
-      t.content = normalize_text(news_doc.at_css('div.editor-contents')&.text)
-      t.posted_on = topic_date
-      t.topic_type_id = TopicType::SMART_AGRI.id
+      topic.content = normalize_text(news_doc.at_css('div.editor-contents')&.text)
+      topic.posted_on = topic_date
+      topic.topic_type_id = TopicType::SMART_AGRI.id
     end
   end
 end
