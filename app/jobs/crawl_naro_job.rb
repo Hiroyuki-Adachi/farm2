33a00745bb52b4297163bfdc2458b5e0be60f1e7
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2025-06-04 18:01:30 +0900

    #598 農研機構

--- app/jobs/crawl_naro_job.rb
+++ app/jobs/crawl_naro_job.rb
@@ -1,12 +1,8 @@
 class CrawlNaroJob < CrawlJob
   queue_as :default
 
-  def perform(words)
+  def perform
     agent = Mechanize.new
-    search_all_agri_news(agent, words)
-  end
-
-  def search_all_agri_news(agent, words)
     top_doc = Nokogiri::HTML(agent.get(URI.join(TopicType::NARO.url, 'new_list.html').to_s).body)
     top_doc.css('div.news dl').children.select(&:element?).each_slice(2) do |dt, dd|
       next unless dt&.name == 'dt' && dd&.name == 'dd'
@@ -17,24 +13,19 @@ class CrawlNaroJob < CrawlJob
       topic_title = dd.at_css('a')&.text
       topic_url = URI.join(TopicType::NARO.url, dd.at_css('a')&.[](:href)).to_s
 
-      save_topic(agent, topic_url, topic_date, topic_title, words)
+      save_topic(agent, topic_url, topic_date, topic_title)
     end
   end
 
   private
 
-  def save_topic(agent, topic_url, topic_date, topic_title, words)
+  def save_topic(agent, topic_url, topic_date, topic_title)
     topic_doc = Nokogiri::HTML(agent.get(topic_url).body)
-    words.each do |word|
-      if topic_doc.text.include?(word)
-        topic = Topic.find_or_create_by(url: topic_url) do |new_topic|
-          new_topic.title = topic_title
-          new_topic.content = normalize_text(topic_doc.css('#postBlock').text)
-          new_topic.posted_on = topic_date
-          new_topic.topic_type_id = TopicType::NARO.id
-        end
-        save_user_topic(word, topic)
-      end
+    Topic.find_or_create_by(url: topic_url) do |new_topic|
+      new_topic.title = topic_title
+      new_topic.content = normalize_text(topic_doc.css('#postBlock').text)
+      new_topic.posted_on = topic_date
+      new_topic.topic_type_id = TopicType::NARO.id
     end
   end
 end
