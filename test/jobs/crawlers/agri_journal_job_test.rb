770cbd4990e6ea95ffd08dfcfa65cd38964ee919
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2025-06-05 18:04:41 +0900

    #598 AGRI JOURNAL (test)

--- test/jobs/crawlers/agri_journal_job_test.rb
+++ test/jobs/crawlers/agri_journal_job_test.rb
@@ -4,26 +4,46 @@ class Crawlers::AgriJournalJobTest < ActiveJob::TestCase
   BODY = Struct.new(:body)
 
   test "クロールして記事を保存できる" do
-    list_html = File.read(Rails.root.join("test/fixtures/html/agrijournal.jp.list.html"))
-    item_html = File.read(Rails.root.join("test/fixtures/html/agrijournal.jp.item.html"))
-    user_word = user_words(:words1)
-
-    agent = stub(:get)
-    agent.stubs(:get).with("#{TopicType::AGRI_JOURNAL.url}/news").returns(BODY.new(list_html))
-    agent.stubs(:get).with(regexp_matches(%r{https://.+})).returns(BODY.new(item_html))
+    stub_agri_journal_page(
+      list: "agrijournal.jp.list.html",
+      item1: "agrijournal.jp.item1.html",
+      item2: "agrijournal.jp.item2.html"
+    )
 
     travel_to Date.new(2024, 10, 25) do
-      job = CrawlAgriJournalJob.new
       assert_difference "Topic.count", +1 do
-        job.send(:search_all_agri_news, agent, [user_word.word])
+        Crawlers::AgriJournalJob.perform_now
       end
   
-      last_topic = Topic.last
-      assert_equal "想定される記事のタイトル", last_topic.title
-      assert_equal "https://agrijournal.jp/information/75181/", last_topic.url
-      assert_equal TopicType::AGRI_JOURNAL.id, last_topic.topic_type_id
-      assert_equal Date.new(2024, 10, 21), last_topic.posted_on
-      assert_includes last_topic.content, "想定される記事の本文の一部"
+      assert_expected_topic
     end
+  end
+
+  private
+
+  def stub_agri_journal_page(list:, item1:, item2:)
+    base_url = TopicType::AGRI_JOURNAL.url
+    stub_request(:get, "#{base_url}/allposts").to_return(
+      body: read_fixture(list)
+    )
+    stub_request(:get, %r{https://agrijournal.jp/information/82779/}).to_return(
+      body: read_fixture(item1)
+    )
+    stub_request(:get, %r{https://agrijournal.jp/renewableenergy/83364/}).to_return(
+      body: read_fixture(item2)
+    )
+  end
+
+  def read_fixture(filename)
+    Rails.root.join("test/fixtures/html/#{filename}").read
+  end
+
+  def assert_expected_topic
+    last_topic = Topic.last
+    assert_equal "想定される記事のタイトル", last_topic.title
+    assert_equal "https://agrijournal.jp/information/82779/", last_topic.url
+    assert_equal TopicType::AGRI_JOURNAL.id, last_topic.topic_type_id
+    assert_equal Date.new(2024, 10, 21), last_topic.posted_on
+    assert_includes last_topic.content, "想定される記事の本文の一部"
   end
 end
