47cb6309eddaa6e8a704efa96160720d942833ff
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2025-06-05 21:28:58 +0900

    #598 SMART AGRI (test)

--- test/jobs/crawlers/smart_agri_job_test.rb
+++ test/jobs/crawlers/smart_agri_job_test.rb
@@ -1,41 +1,43 @@
 require 'test_helper'
 
 class Crawlers::SmartAgriJobTest < ActiveJob::TestCase
-  BODY = Struct.new(:body)
-  
   test "クロールして記事を保存できる" do
-    list_json = File.read(Rails.root.join("test/fixtures/html/smartagri-jp.com.list.json"))
-    item_html = File.read(Rails.root.join("test/fixtures/html/smartagri-jp.com.item.html"))
-    user_word = user_words(:words1)
-    search_params = {count: 30, offset: 0, keyword: user_word.word}
-
-    agent = stub(:get)
-    agent.stubs(:get).with("#{TopicType::SMART_AGRI.url}/api/v2/ajax/post", search_params).returns(BODY.new(list_json))
-    agent.stubs(:get).with(regexp_matches(%r{https://.+})).returns(BODY.new(item_html))
+    stub_pages(
+      list: "smartagri-jp.com.list.json",
+      item: "smartagri-jp.com.item.html"
+    )
 
     travel_to Date.new(2025, 3, 10) do
-      job = CrawlSmartAgriJob.new
       assert_difference "Topic.count", +1 do
-        assert_difference "UserTopic.count", +1 do
-          job.send(:search_all_agri_news, agent, [user_word.word])
-        end
+        Crawlers::SmartAgriJob.perform_now
       end
-  
-      last_topic = Topic.last
-      assert_equal "想定される記事のタイトル", last_topic.title
-      assert_equal "https://smartagri-jp.com/food/10825", last_topic.url
-      assert_equal TopicType::SMART_AGRI.id, last_topic.topic_type_id
-      assert_equal Date.new(2025, 3, 7), last_topic.posted_on
-      assert_includes last_topic.content, "想定される記事の本文の一部"
-  
-      last_user_topic = UserTopic.last
-      assert_equal user_word.user_id, last_user_topic.user_id
-      assert_equal last_topic.id, last_user_topic.topic_id
-      assert_equal user_word.word, last_user_topic.word
-      assert_not last_user_topic.read_flag
-      assert_equal user_word.pc_flag, last_user_topic.pc_flag
-      assert_equal user_word.sp_flag, last_user_topic.sp_flag
-      assert_equal user_word.line_flag, last_user_topic.line_flag
     end
+
+    assert_expected_topic
+  end
+
+  private
+
+  def stub_pages(list:, item:)
+    base_url = TopicType::SMART_AGRI.url
+    stub_request(:get, %r{#{Regexp.escape(base_url)}/api/v2/ajax/post}).to_return(
+      body: read_fixture(list)
+    )
+    stub_request(:get, %r{#{Regexp.escape(base_url)}/food/10825}).to_return(
+      body: read_fixture(item)
+    )
+  end
+
+  def read_fixture(filename)
+    Rails.root.join("test/fixtures/html/#{filename}").read
+  end
+
+  def assert_expected_topic
+    topic = Topic.last
+    assert_equal "想定される記事のタイトル", topic.title
+    assert_equal "https://smartagri-jp.com/food/10825", topic.url
+    assert_equal TopicType::SMART_AGRI.id, topic.topic_type_id
+    assert_equal Date.new(2025, 3, 7), topic.posted_on
+    assert_includes topic.content, "想定される記事の本文の一部"
   end
 end
