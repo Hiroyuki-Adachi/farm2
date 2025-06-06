9a44bdaaaa85dbc42bcb3b0ae945bbd84a47a7fe
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2025-06-07 00:24:18 +0900

    #598 readed flag (SP)

---
+++ app/javascript/controllers/personal_informations/topics.js
@@ -0,0 +1,27 @@
+document.addEventListener('turbo:load', () => {
+    document.querySelectorAll("a.topic-link").forEach(link => {
+        link.addEventListener("click", (event) => {
+            event.preventDefault();
+            fetch(link.dataset.readed, {
+                method: "PATCH",
+                headers: {
+                    "X-CSRF-Token": getCsrfToken()
+                },
+                keepalive: true 
+            })
+            .then(() => window.open(link.href, '_blank'))
+            .catch(() => window.open(link.href, '_blank'))
+            .finally(() => Turbo.visit(window.location.href, { action: 'replace' }));
+        });
+    });
+});
+
+const getCsrfToken = () => {
+    const metas = document.getElementsByTagName('meta');
+    for (let meta of metas) {
+        if (meta.getAttribute('name') === 'csrf-token') {
+            return meta.getAttribute('content');
+        }
+    }
+    return '';
+};
