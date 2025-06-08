93327c6a246a219e63bf98132292ef04b7c57455
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2025-06-08 14:48:21 +0900

    #626 for test

--- test/jobs/news_deliver_job_test.rb
+++ test/jobs/news_deliver_job_test.rb
@@ -2,34 +2,30 @@ require "test_helper"
 
 class NewsDeliverJobTest < ActiveJob::TestCase
   setup do
-    @original = LineHookService.method(:push_messages)
+    @original = LineHookService.method(:push_message)
   end
 
   test "記事のLINE配信に成功した場合" do
-    free_user_topic = user_topics(:free_unread)
-
-    LineHookService.define_singleton_method(:push_messages) do |*args|
+    LineHookService.define_singleton_method(:push_message) do
       Net::HTTPOK.new("1.1", "200", "OK")
     end
 
     perform_enqueued_jobs { NewsDeliverJob.perform_now }
 
-    assert free_user_topic.reload.read_flag
+    assert user_topics(:free_unread).reload.read_flag
   end
 
   test "記事のLINE配信でエラーが発生した場合" do
-    free_user_topic = user_topics(:free_unread)
-  
-    LineHookService.define_singleton_method(:push_messages) do |*args|
+    LineHookService.define_singleton_method(:push_message) do
       Net::HTTPServerError.new(1.0, "500", "Error")
     end
   
     perform_enqueued_jobs { NewsDeliverJob.perform_now }
   
-    assert_not free_user_topic.reload.read_flag
+    assert_not user_topics(:free_unread).reload.read_flag
   end
 
   teardown do
-    LineHookService.define_singleton_method(:push_messages, @original)
+    LineHookService.define_singleton_method(:push_message, @original)
   end
 end
