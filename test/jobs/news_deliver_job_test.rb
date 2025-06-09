36b297f5a59436c791b123dd76f84b529225cacf
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2025-06-09 19:32:24 +0900

    #626 order by

--- test/jobs/news_deliver_job_test.rb
+++ test/jobs/news_deliver_job_test.rb
@@ -2,11 +2,11 @@ require "test_helper"
 
 class NewsDeliverJobTest < ActiveJob::TestCase
   setup do
-    @original = LineHookService.method(:push_message)
+    @original = LineHookService.method(:push_messages)
   end
 
   test "記事のLINE配信に成功した場合" do
-    LineHookService.define_singleton_method(:push_message) do
+    LineHookService.define_singleton_method(:push_messages) do |_line_id, _messages|
       Net::HTTPOK.new("1.1", "200", "OK")
     end
 
@@ -16,7 +16,7 @@ class NewsDeliverJobTest < ActiveJob::TestCase
   end
 
   test "記事のLINE配信でエラーが発生した場合" do
-    LineHookService.define_singleton_method(:push_message) do
+    LineHookService.define_singleton_method(:push_messages) do |_line_id, _messages|
       Net::HTTPServerError.new(1.0, "500", "Error")
     end
   
@@ -26,6 +26,6 @@ class NewsDeliverJobTest < ActiveJob::TestCase
   end
 
   teardown do
-    LineHookService.define_singleton_method(:push_message, @original)
+    LineHookService.define_singleton_method(:push_messages, @original)
   end
 end
