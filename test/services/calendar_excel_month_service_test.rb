4b87f9d3bf74d1e9de6c6d456d103f5091ebba4f
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2025-08-14 16:03:41 +0900

    Revert "Update test/services/calendar_excel_month_service_test.rb"
    This reverts commit 171f28d9cd95c413d1e536f8b113e085e1a77caa.

--- test/services/calendar_excel_month_service_test.rb
+++ test/services/calendar_excel_month_service_test.rb
@@ -17,8 +17,7 @@ class CalendarExcelMonthServiceTest < ActiveSupport::TestCase
     assert_equal work_kinds(:work_kind_taue).name, sheet[3][0].value
     assert_equal work_date.year, sheet[5][0].value
     assert_equal work_date.month, sheet[5][2].value
-    expected_row = calculate_work_entry_row(work_date.day)
-    assert_equal '代掻(10a)', sheet[expected_row][2].value
+    assert_equal '代掻(10a)', sheet[(work_date.day * 2) + 4][2].value
   end
 end
 
