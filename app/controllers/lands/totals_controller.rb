616edee5b23a26c622cc49cfe0a6c5e683c77cc8
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2025-08-30 13:50:47 +0900

    #676 for test

--- app/controllers/lands/totals_controller.rb
+++ app/controllers/lands/totals_controller.rb
@@ -2,8 +2,8 @@ class Lands::TotalsController < ApplicationController
   include PermitChecker
 
   def index
-    @work_kinds = []
+    @work_kind_ids = []
     @work_kind_ids = params[:work_kinds].compact_blank! if params[:work_kinds]
-    @lands = LandTotalQuery.new(@work_kind_ids, current_system).call unless @work_kind_ids.empty?
+    @lands = LandTotalQuery.new(@work_kind_ids, current_system).call if @work_kind_ids.present?
   end
 end
