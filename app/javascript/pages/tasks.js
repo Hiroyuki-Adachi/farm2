0e78ab06fc00bfbc85d6b76ed50f4a12241c1d8a
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2025-09-14 11:24:04 +0900

    #466 importmap修正

--- app/javascript/pages/tasks.js
+++ app/javascript/pages/tasks.js
@@ -1,4 +1,4 @@
 import { application } from "controllers/application"
-import MarkdownPreviewController from "controllers/markdown_preview_controller" 
+import MarkdownPreviewController from "controllers/markdown_preview_controller"
 
 application.register("markdown-preview", MarkdownPreviewController)
