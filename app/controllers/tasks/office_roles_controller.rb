67dc174403274f01581c109fb858aaafce1556e7
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2025-09-15 22:29:57 +0900

    #466 office_role

--- app/controllers/tasks/office_roles_controller.rb
+++ app/controllers/tasks/office_roles_controller.rb
@@ -1,8 +1,22 @@
 class Tasks::OfficeRolesController < TasksController
-  def new
+  before_action :set_task, only: [:edit, :update]
 
+  def edit; end
+
+  def update
+    if @task.update(office_role_params)
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to @task, notice: "タイトルを更新しました" }
+      end
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
-  def create
+  private
+
+  def office_role_params
+    params.expect(task: [:office_role])
   end
 end
