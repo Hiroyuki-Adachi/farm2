1c4f20bffd1a962429bd942e94a9290b910e767f
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2025-09-18 12:29:59 +0900

    #466 test for task/office_role controller

--- test/controllers/tasks/office_roles_controller_test.rb
+++ test/controllers/tasks/office_roles_controller_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class Tasks::OfficeRolesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:users1)
+    login_as(@user)
+    @task = tasks(:open_task)
+  end
+
+  test "部門区分編集" do
+    get edit_task_office_role_path(@task)
+    assert_response :success
+    assert_select "form[action='#{task_office_role_path(@task)}']", true
+  end
+
+  test "部門区分編集(実行)" do
+    @task.update!(office_role: :finance)
+
+    assert_no_difference "TaskEvent.count" do
+      assert_no_difference "Task.count" do
+        patch task_office_role_path(@task),
+              params: { task: { office_role: :general } },
+              headers: { "Accept" => "text/vnd.turbo-stream.html" }
+      end
+    end
+    assert_response :success
+    assert_equal "text/vnd.turbo-stream.html", response.media_type
+    assert_includes response.body, "<turbo-stream"
+
+    @task.reload
+    assert_equal :general, @task.office_role.to_sym
+  end
 end
