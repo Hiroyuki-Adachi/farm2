a20c957db5e25ad7dc9ee6909806ac11770dc1af
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2025-10-19 21:21:23 +0900

    #8 test for model

--- test/models/work_category_test.rb
+++ test/models/work_category_test.rb
@@ -12,7 +12,46 @@
 require "test_helper"
 
 class WorkCategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "未削除の作業ジャンルが1件でも存在する場合" do
+    category = work_categories(:category_rice)
+    assert_no_difference("WorkGenre.kept.count") do
+      assert_no_difference("WorkCategory.kept.count") do
+        assert_raises(ActiveRecord::RecordNotDestroyed) do
+          category.remove_by_policy!
+        end
+      end
+    end
+
+    assert category.reload.kept?
+  end
+
+  test "作業ジャンルが存在するが全て論理削除済み" do
+    result = nil
+    category = work_categories(:category_discardable)
+    assert_no_difference("WorkGenre.kept.count") do
+      assert_no_difference("WorkCategory.count") do
+        result = category.remove_by_policy!
+      end
+    end
+
+    assert_equal :discarded, result
+    assert category.reload.discarded?
+  end
+
+  test "作業ジャンルが1件も存在しない" do
+    result = nil
+    category = work_categories(:category_deletable)
+
+    assert_difference("WorkCategory.count", -1) do
+      result = category.remove_by_policy!
+    end
+    assert_equal :destroyed, result
+    assert_not WorkCategory.exists?(category.id)
+  end
+
+  test "物理削除ブレーキ" do
+    category = work_categories(:category_rice)
+    assert_raises(ActiveRecord::RecordNotDestroyed) { category.destroy! }
+    assert category.reload.kept?
+  end
 end
--- test/models/work_genre_test.rb
+++ test/models/work_genre_test.rb
@@ -21,7 +21,46 @@
 require "test_helper"
 
 class WorkGenreTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "未削除の作業分類が1件でも存在する場合" do
+    genre = work_genres(:genre_rice)
+    assert_no_difference("WorkType.kept.count") do
+      assert_no_difference("WorkGenre.kept.count") do
+        assert_raises(ActiveRecord::RecordNotDestroyed) do
+          genre.remove_by_policy!
+        end
+      end
+    end
+
+    assert genre.reload.kept?
+  end
+
+  test "作業分類が存在するが全て論理削除済み" do
+    result = nil
+    genre = work_genres(:genre_discardable)
+    assert_no_difference("WorkType.kept.count") do
+      assert_no_difference("WorkGenre.count") do
+        result = genre.remove_by_policy!
+      end
+    end
+
+    assert_equal :discarded, result
+    assert genre.reload.discarded?
+  end
+
+  test "作業分類が1件も存在しない" do
+    result = nil
+    genre = work_genres(:genre_deletable)
+
+    assert_difference("WorkGenre.count", -1) do
+      result = genre.remove_by_policy!
+    end
+    assert_equal :destroyed, result
+    assert_not WorkGenre.exists?(genre.id)
+  end
+
+  test "物理削除ブレーキ" do
+    genre = work_genres(:genre_rice)
+    assert_raises(ActiveRecord::RecordNotDestroyed) { genre.destroy! }
+    assert genre.reload.kept?
+  end
 end
