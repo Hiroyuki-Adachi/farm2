7e61f3d5887c83363c5aa71a1ff8d539de381d7a
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2026-05-02 17:11:36 +0900

    #10 for test

--- test/models/system_scoped_join_test.rb
+++ test/models/system_scoped_join_test.rb
@@ -3,29 +3,20 @@ require 'test_helper'
 class SystemScopedJoinTest < ActiveSupport::TestCase
   test "work by_target does not duplicate rows for another organization system" do
     work = works(:works1)
-    scope = Work.by_target(2015)
 
-    assert_includes scope.pluck(:id), work.id
-    assert_equal scope.distinct.pluck(:id).sort, scope.pluck(:id).sort
-    assert_empty scope.where.not(organization_id: work.organization_id).pluck(:id)
+    assert_equal [work.id], Work.by_target(2015).where(id: work.id).pluck(:id)
   end
 
   test "work result by_home does not duplicate rows for another organization system" do
     work_result = work_results(:work_results0)
-    scope = WorkResult.by_home(2015)
 
-    assert_includes scope.pluck(:id), work_result.id
-    assert_equal scope.distinct.pluck(:id).sort, scope.pluck(:id).sort
-    assert_empty scope.where.not(work_results: {organization_id: work_result.organization_id}).pluck(:id)
+    assert_equal [work_result.id], WorkResult.by_home(2015).where(work_results: {id: work_result.id}).pluck(:id)
   end
 
   test "machine result by_home does not duplicate rows for another organization system" do
     machine_result = MachineResult.by_home(2015).first
-    scope = MachineResult.by_home(2015)
 
-    assert_includes scope.pluck(:id), machine_result.id
-    assert_equal scope.distinct.pluck(:id).sort, scope.pluck(:id).sort
-    assert_empty scope.where.not(machine_results: {organization_id: machine_result.organization_id}).pluck(:id)
+    assert_equal [machine_result.id], MachineResult.by_home(2015).where(machine_results: {id: machine_result.id}).pluck(:id)
   end
 
   test "machine result by_home can be scoped by organization" do
@@ -38,11 +29,8 @@ class SystemScopedJoinTest < ActiveSupport::TestCase
 
   test "work chemical by_term does not duplicate rows for another organization system" do
     work_chemical = work_chemicals(:work_chemical1)
-    scope = WorkChemical.by_term(2015)
 
-    assert_includes scope.pluck(:id), work_chemical.id
-    assert_equal scope.distinct.pluck(:id).sort, scope.pluck(:id).sort
-    assert_empty scope.where.not(work_chemicals: {organization_id: work_chemical.organization_id}).pluck(:id)
+    assert_equal [work_chemical.id], WorkChemical.by_term(2015).where(work_chemicals: {id: work_chemical.id}).pluck(:id)
   end
 
   test "work chemical by_term can be scoped by organization" do
