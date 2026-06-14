539be083a429b68c7d6b97807337892a5e022d46
Hiroyuki Adachi <18182113+Hiroyuki-Adachi@users.noreply.github.com>
2026-06-14 11:18:18 +0900

    #901 phase.10

--- test/controllers/users/permissions_controller_test.rb
+++ test/controllers/users/permissions_controller_test.rb
@@ -21,4 +21,19 @@ class Users::PermissionsControllerTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal user[:permission_id], @user.permission_id.to_sym
   end
+
+  test "他組織ユーザの権限変更画面は表示しない" do
+    get new_user_permission_path(user_id: users(:user_admin_org2).id)
+
+    assert_response :service_unavailable
+  end
+
+  test "他組織ユーザの権限は変更しない" do
+    other_user = users(:user_admin_org2)
+
+    assert_no_changes -> { other_user.reload.permission_id } do
+      post user_permissions_path(user_id: other_user.id), params: { user: { permission_id: :manager } }
+    end
+    assert_response :service_unavailable
+  end
 end
