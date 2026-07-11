33225d74061977e3e125e2f062e02e9e28957853
博之 足立 <Hiroyuki-Adachi@users.noreply.github.com>
2026-07-12 00:49:02 +0900

    #10 phase.2 term_nameを年度表示に使用

--- test/integration/change_term_test.rb
+++ test/integration/change_term_test.rb
@@ -24,7 +24,7 @@ class ChangeTermTest < ActionDispatch::IntegrationTest
     new_system = System.find_by(term: new_term, organization_id: @organization.id)
     get menu_index_path
     assert_response :success
-    assert_select 'h1', /#{new_system.start_date.strftime('%Jy年')}/
+    assert_select 'h1', /#{new_system.term_name}/
   end
 
   def teardown
