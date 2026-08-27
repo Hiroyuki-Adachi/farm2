7c79b1345b3c00128bf4a9f1d2067e19c09622d0
博之 足立 <Hiroyuki-Adachi@users.noreply.github.com>
2026-08-27 21:00:20 +0900

    #1165 タブレットメニューテストのログイン処理を簡素化

--- test/controllers/tablets/menu_controller_test.rb
+++ test/controllers/tablets/menu_controller_test.rb
@@ -2,16 +2,7 @@ require "test_helper"
 
 class Tablets::MenuControllerTest < ActionDispatch::IntegrationTest
   setup do
-    user = users(:users1)
-    qr_login_session = QrLoginSession.create!(
-      status: :approved,
-      user_id: user.id,
-      expires_at: 5.minutes.from_now
-    )
-
-    post consume_sessions_qr_login_path(qr_login_session.token),
-         params: { redirect_to: tablets_menu_index_path },
-         headers: { "ACCEPT" => "application/json" }
+    login_as(users(:users1))
   end
 
   test "ログアウトリンクはTurboプリフェッチを無効にする" do
